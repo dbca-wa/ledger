8468fdb10f4bb94502134dd83392f8871d99c71d
tony <tony@gaiaresources.com.au>
2016-02-10 10:53:19 +0800

    Created content in index.html, namely login panel, which is working. Replaced tabs with spaces.

--- wildlifelicensing/urls.py
+++ wildlifelicensing/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
 
 urlpattern = [
-     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url('^login/', auth_views.login, {'template_name': 'index.html'}, name='login'),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), {'form': AuthenticationForm}),
 ]
