f919a1ab8e62b638a35025f01bdd229a5678fe27
serge <serge@gaiaresources.com.au>
2016-02-16 14:05:14 +0800

    Removed all urls/templates associated with the password reset feature.

--- wildlifelicensing/apps/accounts/urls.py
+++ wildlifelicensing/apps/accounts/urls.py
@@ -9,28 +9,4 @@ urlpatterns = [
     url('^logout/', auth_views.logout, {'next_page': 'home'}, name='logout'),
 
     url(r'create/?$', views.UserCreateView.as_view(), name='user_create'),
-
-    url(r'^password_reset/done/$',
-        auth_views.password_reset_done,
-        {'template_name': 'password_reset_done.html'},
-        name='password_reset_done'),
-
-    url(r'^password_reset/$',
-        auth_views.password_reset,
-        {'template_name': 'password_reset_form.html',
-         'email_template_name': 'password_reset_email.html'},
-        name='password_reset'),
-
-
-    url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        auth_views.password_reset_confirm,
-        {'template_name': 'password_reset_confirm.html',
-         'post_reset_redirect': 'password_reset_complete'},
-        name='password_reset_confirm'),
-
-    url(r'^password_reset/complete/$',
-        auth_views.password_reset_complete,
-        {'template_name': 'password_reset_complete.html'},
-        name='password_reset_complete'),
-
 ]
