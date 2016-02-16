49118cdb1121a39fe4c0607169cfd7b1ca08f307
tony <tony@gaiaresources.com.au>
2016-02-16 13:01:44 +0800

    Fixed to work with new settings.

--- wildlifelicensing/urls.py
+++ wildlifelicensing/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from django.contrib.auth.forms import AuthenticationForm
 
-urlpattern = [
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html"), {'form': AuthenticationForm}, name='home'),
-    url(r'^accounts/', include('wildlifelicensing.apps.accounts.urls')),
+    url(r'^accounts/', include('wildlifelicensing.apps.accounts.urls', namespace='accounts')),
     url(r'^applicants/', include('wildlifelicensing.apps.applicants.urls', namespace='applicants')),
     url(r'^officers/', include('wildlifelicensing.apps.officers.urls', namespace='officers')),
 ]
