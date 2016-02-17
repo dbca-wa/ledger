98e7e34f1f0a1cb987c90f0f8fd74fb7449d22e1
serge <serge@gaiaresources.com.au>
2016-02-17 11:24:25 +0800

    Customer model draft

--- wildlifelicensing/apps/accounts/models.py
+++ wildlifelicensing/apps/accounts/models.py
@@ -7,16 +7,53 @@ from django.utils.encoding import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
-class Persona(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True, verbose_name='Persona Name',
-                            help_text='Persona Name')
-    email = models.EmailField(null=True, blank=True, verbose_name='Email', help_text='Email')
-    user = models.ForeignKey(EmailUser, null=False, blank=False)
-    address = models.ForeignKey(Address, null=True, blank=True)
-
-    def __str__(self):
-        return self.name if self.name else self.email
+class Document(models.Model):
+    name = models.CharField(max_length=100, blank=True,
+                            verbose_name='name', help_text='')
+    description = models.TextField(blank=True,
+                                   verbose_name='description', help_text='')
+    file = models.FileField(upload_to='%Y/%m/%d')
+    uploaded_date = models.DateTimeField(auto_now_add=True)
 
     @property
-    def email(self):
-        return self.email or self.user.email
+    def path(self):
+        return self.file.path
+
+    @property
+    def filename(self):
+        return self.path.basename(self.path)
+
+    def __str__(self):
+        return self.name or self.filename
+
+
+@python_2_unicode_compatible
+class Customer(EmailUser):
+    TITLE_CHOICES = (
+        ('Mr', 'Mr'),
+        ('Miss', 'Miss'),
+        ('Mrs', 'Mrs'),
+        ('Ms', 'Ms'),
+        ('Dr', 'Dr')
+    )
+    title = models.CharField(max_length=100, choices=TITLE_CHOICES, blank=False, default=TITLE_CHOICES[0][0],
+                             verbose_name='title', help_text="")
+    dob = models.DateField(auto_now=False, auto_now_add=False, null=False, blank=False,
+                           verbose_name="date of birth", help_text='')
+    phone_number = models.CharField(max_length=50, null=True, blank=True,
+                                    verbose_name="phone number", help_text='')
+    mobile_number = models.CharField(max_length=50, null=True, blank=True,
+                                     verbose_name="mobile number", help_text='')
+    fax_number = models.CharField(max_length=50, null=True, blank=True,
+                                  verbose_name="fax number", help_text='')
+    organisation = models.CharField(max_length=300, null=True, blank=True,
+                                    verbose_name="organisation", help_text='organisation, institution or company')
+
+    residential_address = models.ForeignKey(Address, null=False, blank=False, related_name='+')
+    postal_address = models.ForeignKey(Address, null=True, blank=True, related_name='+')
+    billing_address = models.ForeignKey(Address, null=True, blank=True, related_name='+')
+
+    documents = models.ManyToManyField(Document)
+
+    def __str__(self):
+        return self.email
