8cd3e8c3b350c52ae60bedb9221c44b4197d4857
Tony Prior <tony@gaiaresources.com.au>
2016-05-10 21:20:10 +0800

    Added missing js file.

---
+++ wildlifelicensing/apps/applications/static/wl/js/issue/issue_licence.js
@@ -0,0 +1,17 @@
+define(['jQuery', 'bootstrap-datetimepicker'], function($) {
+    return {
+        initialise: function() {
+            // initialise all datapickers
+//            $("input[id$='date']").each(function() {
+//                $(this).datetimepicker({
+//                    format: 'DD/MM/YYYY'
+//                });
+//            });
+
+            $('#issue').click(function(e) {
+                var $issueLicenceForm = $('#issueLicenceForm');
+                $issueLicenceForm.submit();
+            });
+        }
+    }
+});
