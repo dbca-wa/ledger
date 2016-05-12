5ba93a57bfc7021b15f27262136c27c4899b16eb
Rocky Chen <Rocky.Chen@dpaw.wa.gov.au>
2016-05-12 11:41:18 +0800

    Maintain email identity for user profile

--- wildlifelicensing/apps/main/static/wl/js/profiles_table.js
+++ wildlifelicensing/apps/main/static/wl/js/profiles_table.js
@@ -6,6 +6,15 @@ define(['jQuery', 'js/wl.dataTable'], function($, dataTable) {
             }, [
                 {title: 'Display Name', data: 'name'},
                 {title: 'Email', data: 'email'},
+                /*
+                {title: 'Auth Identity', data: 'auth_identity',render:function(data,type,row) {
+                    if (data) {
+                        return '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span'
+                    } else {
+                        return '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span'
+                    }
+                }},
+                */
                 {title: 'Institution', data: 'institution'},
                 {title: 'Postal Address', data: 'postal_address.search_text'},
                 {title: 'Action', data: 'id', render: function(data, type, row) {
--- wildlifelicensing/apps/main/views.py
+++ wildlifelicensing/apps/main/views.py
@@ -8,7 +8,7 @@ from django.views.generic.edit import FormView
 
 from preserialize.serialize import serialize
 
-from ledger.accounts.models import Profile, Document
+from ledger.accounts.models import Profile, Document,EmailIdentity
 from ledger.accounts.forms import AddressForm, ProfileForm
 
 from forms import IdentificationForm
@@ -23,6 +23,11 @@ class ListProfilesView(CustomerRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(ListProfilesView, self).get_context_data(**kwargs)
 
+        def posthook(instance,attr):
+            attr["auth_identity"] = instance.auth_identity
+            return attr
+
+        #context['data'] = serialize(Profile.objects.filter(user=self.request.user),posthook=posthook)
         context['data'] = serialize(Profile.objects.filter(user=self.request.user))
 
         return context
@@ -45,6 +50,12 @@ class CreateProfilesView(CustomerRequiredMixin, TemplateView):
             profile.postal_address = address_form.save()
             profile.user = request.user
             profile.save()
+            #maintain email identity
+            email = profile.email
+            auth_identity = profile_form.cleaned_data.get("auth_identity",False)
+            if auth_identity and email:
+                identity, created = EmailIdentity.objects.get_or_create(email=email, user=profile.user)
+
         else:
             return render(request, self.template_name, {'profile_form': profile_form,
                                                         'address_form': address_form})
@@ -72,13 +83,28 @@ class EditProfilesView(CustomerRequiredMixin, TemplateView):
 
         if profile.user != request.user:
             return HttpResponse('Unauthorized', status=401)
-
+        origin_email = profile.email
+        origin_auth_identity = profile.auth_identity
         profile_form = ProfileForm(request.POST, instance=profile)
         address_form = AddressForm(request.POST, instance=profile.postal_address)
 
         if profile_form.is_valid() and address_form.is_valid():
             profile.save()
             address_form.save()
+            email = profile.email
+            #maintain email identity
+            auth_identity = profile_form.cleaned_data.get("auth_identity",False)
+            if origin_email != email:
+                if origin_auth_identity:
+                    #user changed the profile's email, remove the email from EmailIdentity.
+                    EmailIdentity.objects.filter(user=profile.user,email=origin_email).delete()
+                if auth_identity and email:
+                    identity, created = EmailIdentity.objects.get_or_create(email=email, user=profile.user)
+            elif origin_auth_identity != auth_identity and email:
+                if auth_identity:
+                    identity, created = EmailIdentity.objects.get_or_create(email=email, user=profile.user)
+                else:
+                    EmailIdentity.objects.filter(user=profile.user,email=origin_email).delete()
         else:
             return render(request, self.template_name, {'profile_form': ProfileForm(instance=profile),
                                                         'address_form': AddressForm(instance=profile.postal_address)})
