8f7a08063383e07e293ba2a76c82a54f19c5c695
tony <tony@gaiaresources.com.au>
2016-06-07 14:51:53 +0800

    Logging not required for this type of email.

--- wildlifelicensing/apps/returns/emails.py
+++ wildlifelicensing/apps/returns/emails.py
@@ -1,11 +1,8 @@
 import logging
 
-from django.core.mail import EmailMultiAlternatives, EmailMessage
-
 from django_hosts import reverse as hosts_reverse
 
 from wildlifelicensing.apps.emails.emails import TemplateEmailBase
-from wildlifelicensing.apps.applications.models import EmailLogEntry
 
 logger = logging.getLogger(__name__)
 
@@ -26,32 +23,3 @@ def send_return_overdue_email_notification(ret):
     }
 
     email.send(ret.licence.profile.email, context=context)
-
-
-def _log_email(email_message, application, sender=None):
-    if isinstance(email_message, (EmailMultiAlternatives, EmailMessage,)):
-        # TODO this will log the plain text body, should we log the html instead
-        text = email_message.body
-        subject = email_message.subject
-        from_email = unicode(sender) if sender else unicode(email_message.from_email)
-        # the to email is normally a list
-        if isinstance(email_message.to, list):
-            to = ';'.join(email_message.to)
-        else:
-            to = unicode(email_message.to)
-    else:
-        text = unicode(email_message)
-        subject = ''
-        to = application.applicant_profile.user.email
-        from_email = unicode(sender) if sender else ''
-
-    kwargs = {
-        'subject': subject,
-        'text': text,
-        'application': application,
-        'user': sender,
-        'to': to,
-        'from_email': from_email
-    }
-    email_entry = EmailLogEntry.objects.create(**kwargs)
-    return email_entry
