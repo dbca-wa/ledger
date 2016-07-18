26ecead3f501233a716394ce72068b8b741990cd
serge <serge@gaiaresources.com.au>
2016-07-18 17:31:06 +0800

    SpeciesName view: -improved speed by narrowing the returned properties. - added the type['flora', 'fauna'] filter

--- ledger/taxonomy/views.py
+++ ledger/taxonomy/views.py
@@ -6,14 +6,28 @@ from django.views.generic import View
 from django.conf import settings
 
 
+def add_filter(cql_filter, params):
+    if 'cql_filter' not in params:
+        params['cql_filter'] = cql_filter
+    else:
+        params['cql_filter'] = params['cql_filter'] + ' AND ' + cql_filter
+
+
 class SpeciesNamesJSON(View):
+
     def get(self, request, *args, **kwargs):
         base_url = settings.HERBIE_SPECIES_WFS_URL
-        params = {'sortBy': 'species_name'}
+        params = {'propertyName': 'species_name', 'sortBy': 'species_name'}
         # search filter. The search is equivalent to a 'icontains' query.
         search = request.GET.get('search')
         if search:
-            params['cql_filter'] = "species_name ILIKE '%{}%'".format(search)
+            filter_ = "species_name ILIKE '%{}%'".format(search)
+            add_filter(filter_, params)
+        kingdom = request.GET.get('type')
+        if kingdom == 'fauna':
+            add_filter('kingdom_id in (5)', params)
+        elif kingdom == 'flora':
+            add_filter('kingdom_id in (3)', params)
         r = requests.get(base_url, params=params)
         names = []
         if r.status_code == 200:
