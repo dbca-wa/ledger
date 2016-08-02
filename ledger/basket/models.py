e5d304146aa2f097c6e40d1db7453d21bac0f9e1
Brian <ndwigabrian@gmail.com>
2016-08-02 10:03:07 +0800

    added system id to the basket

--- ledger/basket/models.py
+++ ledger/basket/models.py
@@ -1,1 +1,7 @@
+from django.db import models
+from oscar.apps.basket.abstract_models import AbstractBasket as CoreAbstractBasket
+
+class Basket(CoreAbstractBasket):
+    system = models.CharField(max_length=4,blank=True,null=False)
+
 from oscar.apps.basket.models import *  # noqa
