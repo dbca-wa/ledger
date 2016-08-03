bfce654ea1ebe56e11b82257bc29b86ac5f249a1
Brian <ndwigabrian@gmail.com>
2016-08-03 14:39:51 +0800

    added oracle_code to line

--- ledger/order/models.py
+++ ledger/order/models.py
@@ -1,1 +1,8 @@
+from django.db import models
+from oscar.apps.order.abstract_models import AbstractLine as CoreAbstractLine
+
+class Line(CoreAbstractLine):
+
+    oracle_code = models.CharField("Oracle Code",max_length=50,null=True,blank=True)
+
 from oscar.apps.order.models import *  # noqa
