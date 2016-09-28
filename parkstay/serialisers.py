49e6800ac5a00cb9f54f6a758af3e14ae3ba759d
Brian Ndwiga <ndwigabrian@gmail.com>
2016-09-28 13:45:00 +0800

    created serializers and endpoints for all models

--- parkstay/serialisers.py
+++ parkstay/serialisers.py
@@ -1,18 +1,44 @@
-from parkstay.models import CampsiteBooking, Campsite, Campground, Park
+from parkstay.models import CampsiteBooking, Campsite, Campground, Park, PromoArea, CampgroundFeature, Region, CampsiteClass, Booking, CampsiteRate
 from rest_framework import serializers
+
+class ParkSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Park
+
+class PromoAreaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+            model = PromoArea
+
+class CampgroundSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Campground
+
+class CampsiteSerialiser(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Campsite
+        fields = ('campground', 'name', 'campsite_class', 'features', 'max_people')
+
+class CampgroundFeatureSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+                model = CampgroundFeature
+
+class RegionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+                model = Region
+
+class CampsiteClassSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+                model = CampsiteClass
 
 class CampsiteBookingSerialiser(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CampsiteBooking
         fields = ('campsite', 'date', 'booking_type')
 
-class CampsiteSerialiser(serializers.HyperlinkedModelSerializer):
+class BookingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Campsite
-        fields = ('campground', 'name', 'campsite_class', 'features', 'max_people')
-"""
-class CampgroundSerialiser((Serializer.HyperlinkedModelSerializer)
+                model = Booking
+
+class CampsiteRateSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Campground
-        fields = ('', '', '', '', '')
-"""
+                model = CampsiteRate
