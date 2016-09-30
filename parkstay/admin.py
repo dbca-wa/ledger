91db1ae97ff527305042faa889ded5d059d05c4d
Brian Ndwiga <ndwigabrian@gmail.com>
2016-09-30 09:20:55 +0800

    order campsite class alphabetically

--- parkstay/admin.py
+++ parkstay/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Park, Campground, Campsite, CampgroundFeature, Region, CampsiteClass, CampsiteBooking, Booking,PromoArea
 
+@admin.register(CampsiteClass)
+class CampsiteClassAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
 admin.site.register(Park)
 admin.site.register(Campground)
 admin.site.register(Campsite)
 admin.site.register(CampgroundFeature)
 admin.site.register(Region)
-admin.site.register(CampsiteClass)
 admin.site.register(CampsiteBooking)
 admin.site.register(Booking)
 admin.site.register(PromoArea)
