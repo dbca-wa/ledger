b2c2a759c2b6b3a3dba41305e06614127715c39b
Brian Ndwiga <ndwigabrian@gmail.com>
2016-11-02 15:13:05 +0800

    parkstay: define vue router better

--- parkstay/frontend/parkstay/src/main.js
+++ parkstay/frontend/parkstay/src/main.js
@@ -7,13 +7,42 @@ import Router from 'vue-router'
 
 Vue.use(Router);
 
+global.debounce = function (func, wait, immediate) {
+    // Returns a function, that, as long as it continues to be invoked, will not
+    // be triggered. The function will be called after it stops being called for
+    // N milliseconds. If `immediate` is passed, trigger the function on the
+    // leading edge, instead of the trailing.
+    'use strict';
+    var timeout;
+    return function () {
+        var context = this;
+        var args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    }
+};
+
 const routes = [
-  { path: '/', component: Campgrounds }
+    {
+        path: '/',
+        component: {
+            render (c) { return c('router-view') }
+        },
+        children: [
+            {path:'dashboard',component: Campgrounds}
+        ]
+    }
 ];
 
 const router = new Router({
   'routes' : routes,
-  'history': true
+  'mode': 'history'
 });
 
 new Vue({
