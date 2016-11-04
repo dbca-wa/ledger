e6434fa576ee086f1e2aa8a3b22348fa57966bc5
tawanda <tawanda.nyakudjga@gmail.com>
2016-11-04 17:00:46 +0800

    create campground attributes component

--- parkstay/frontend/parkstay/src/components/campgrounds/campground.vue
+++ parkstay/frontend/parkstay/src/components/campgrounds/campground.vue
@@ -2,51 +2,50 @@
 
    <div class="panel-group" id="applications-accordion" role="tablist" aria-multiselectable="true">
       <div class="panel panel-default" id="applications">
-           <div class="panel-heading" role="tab" id="applications-heading">
-               <h4 class="panel-title">
-                   <a role="button" data-toggle="collapse" href="#applications-collapse"
-                      aria-expanded="true" aria-controls="collapseOne">
-                       <h3>{{title}}</h3>
-                   </a>
-               </h4>
-           </div>
-           <div id="applications-collapse" class="panel-collapse collapse in" role="tabpanel"
-                aria-labelledby="applications-heading">
-               <div class="panel-body">
-                  <div class="col-lg-12">
-                     <div class="row">
-                        <div class="well">
-                           <h1>Campground attributes</h1>
-                        </div>
+        <div class="panel-heading" role="tab" id="applications-heading">
+            <h4 class="panel-title">
+                <a role="button" data-toggle="collapse" href="#applications-collapse"
+                   aria-expanded="true" aria-controls="collapseOne">
+                    <h3>{{title}}</h3>
+                </a>
+            </h4>
+        </div>
+        <div id="applications-collapse" class="panel-collapse collapse in" role="tabpanel"
+             aria-labelledby="applications-heading">
+            <div class="panel-body">
+               <div class="col-lg-12">
+                  <div class="row">
+                     <campgroundAttr @create="create" >
+                        <h1 slot="cg_name">My Slot</h1>
+                     </campgroundAttr>
+                  </div>
+                  <div class="row">
+                     <div class="well">
+                        <h1>Price History</h1>
                      </div>
-                     <div class="row">
-                        <div class="well">
-                           <h1>Price History</h1>
-                        </div>
-                     </div>
-                     <div class="row">
-                        <div class="well">
-                           <datatable :dtHeaders ="headers" :dtOptions="options"/>
-                        </div>
+                  </div>
+                  <div class="row">
+                     <div class="well">
+                        <datatable :dtHeaders ="headers" :dtOptions="options"/>
                      </div>
                   </div>
                </div>
-           </div>
+            </div>
+         </div>
       </div>
    </div>
-
-
-
 
 </template>
 
 <script>
 import datatable from '../utils/datatable.vue'
+import campgroundAttr from './campground-attr.vue'
 import {$,Moment} from '../../hooks.js'
+
 export default {
    name:'campground',
    components:{
-      datatable
+      datatable,campgroundAttr
    },
    data:function () {
       return {
@@ -87,6 +86,12 @@ export default {
          headers:['Closure Start','Reopen', 'Closure Reason', 'Action'],
          title: 'Campground'
       }
+   },
+   methods:{
+      create:function () {
+         console.log('create in Campground');
+         alert('Create was clicked')
+      }
    }
 }
 </script>
@@ -94,5 +99,8 @@ export default {
 <style lang="css">
 .well{
    background-color: #fff;
+}
+.btn{
+   margin-bottom: 10px;
 }
 </style>
