d8ff3d77ac683732fe2d6c9535d654a56939fdd3
Brian Ndwiga <ndwigabrian@gmail.com>
2016-11-08 14:26:25 +0800

    parkstay: added datetimepicker in hooks

--- parkstay/frontend/parkstay/src/hooks.js
+++ parkstay/frontend/parkstay/src/hooks.js
@@ -1,15 +1,16 @@
 // module for all third party dependencies
 
 import $ from 'jquery'
-
 var DataTable = require( 'datatables.net' )();
 var DataTableBs = require( 'datatables.net-bs' )();
 var DataTableRes = require( 'datatables.net-responsive-bs' )();
 var Moment = require('moment');
+var datetimepicker = require('datetimepicker');
 export {
     $,
     DataTable,
     DataTableBs,
     DataTableRes,
-    Moment
+    Moment,
+    datetimepicker
 }
