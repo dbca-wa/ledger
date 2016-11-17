6c2fe4846fbd7c9a59668187070fd631d5e2b0f6
tawanda <tawanda.nyakudjga@gmail.com>
2016-11-17 16:43:51 +0800

    nicer 404 page

--- parkstay/frontend/parkstay/src/components/utils/404.vue
+++ parkstay/frontend/parkstay/src/components/utils/404.vue
@@ -1,15 +1,14 @@
 <template lang="html" id="error-404">
-  <div> 
-    <div class="container">
-      <div class="row" >
-        <div class="col-lg-12 alert alert-danger">
-            <h1 class="text-center"> 404 Not Found </h1>
-        </div>
-        <div class="col-lg-12" style="margin-top:20px;">
-          <slot></slot>
-        </div>
+  <div class="wrapper">
+      <div>
+          <h1>Opps 404 not found</h1>
+          <p>
+              The page you are looking for could not be found.<br/>
+              Check the address bar to ensure your URL is spelled correctly.
+              If all else fails, you can visit our dashboard page at the link below.
+          </p>
+          <router-link :to="{ name: 'cg_main'}">Visit Dashboard Page</router-link>
       </div>
-    </div>
   </div>
 </template>
 
@@ -20,4 +19,17 @@ export default {
 </script>
 
 <style lang="css">
+    .wrapper{
+        display: flex;
+        justify-content: left center;
+        align-items: center;
+        min-height: 100%;
+        min-width:100%;
+        position: absolute;
+        bottom:10px;
+        color: #333;
+    }
+    .wrapper h1{
+        font-size: 4em;
+    }
 </style>
