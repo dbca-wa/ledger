6c41ca0f89446fe6d80a14bdfa3da6fe464fd94c
Brian Ndwiga <ndwigabrian@gmail.com>
2016-11-23 09:39:15 +0800

    parkstay: create or update campground history view if the price level is at campground_level

--- parkstay/models.py
+++ parkstay/models.py
@@ -822,9 +822,10 @@ class CampsiteRateListener(object):
         original_instance = getattr(instance, "_original_instance") if hasattr(instance, "_original_instance") else None
         try:
             cursor = connection.cursor()
-            sql =   'CREATE OR REPLACE VIEW parkstay_campground_pricehistory_v AS \
+            if insance.campsite.campground.price_level == 0:
+                sql =   'CREATE OR REPLACE VIEW parkstay_campground_pricehistory_v AS \
                 SELECT distinct camps.campground_id as id,cr.date_start,cr.date_end, r.id as rate_id, r.adult, r.concession, r.child from parkstay_campsiterate cr INNER JOIN parkstay_rate r on r.id= cr.rate_id  INNER JOIN \
-                (SELECT cg.id AS campground_id,cs.name AS name,cs.id AS campsite_id from  parkstay_campsite cs, parkstay_campground cg WHERE cs.campground_id = cg.id and cg.id = cs.campground_id) camps ON cr.campsite_id = camps.campsite_id'
+                (SELECT cg.id AS campground_id,cs.name AS name,cs.id AS campsite_id from  parkstay_campsite cs, parkstay_campground cg WHERE cs.campground_id = cg.id and cg.id = cs.campground_id and cg.price_level = 0) camps ON cr.campsite_id = camps.campsite_id'
             cursor.execute(sql)
         except Exception as e:
             raise ValidationError(e)
