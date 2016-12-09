d5634e015265c4df641455cbb8408b41c6ffd295
Brian Ndwiga <ndwigabrian@gmail.com>
2016-12-09 12:52:19 +0800

    parkstay: fix issue for displaying other

--- parkstay/frontend/parkstay/src/components/utils/closureHistory.vue
+++ parkstay/frontend/parkstay/src/components/utils/closureHistory.vue
@@ -65,7 +65,8 @@ export default {
             campsite:{},
             closure: {
                 id:'',
-                status: 1
+                status: 1,
+                reason: ''
             },
             deleteClosure: null,
             deleteClosurePrompt: {
