77cc3504ffff244012d75ec1146040e59999a30f
tawanda <tawanda.nyakudjga@gmail.com>
2016-12-19 13:38:49 +0800

    add date rangepicker library

--- parkstay/frontend/parkstay/src/hooks-css.js
+++ parkstay/frontend/parkstay/src/hooks-css.js
@@ -4,3 +4,4 @@ require("datatables.net-bs/css/dataTables.bootstrap.css");
 require("slick-carousel-browserify/slick/slick.css");
 require("select2/dist/css/select2.min.css");
 require("select2-bootstrap-theme/dist/select2-bootstrap.min.css");
+require('bootstrap-daterangepicker/daterangepicker.css');
