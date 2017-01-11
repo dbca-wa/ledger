3ad5c48a39d4c0cff8c7e1c068eb7869f67d9854
tawanda <tawanda.nyakudjga@gmail.com>
2017-01-11 12:38:47 +0800

    Parkstay: dreadonly fields for dates and guests

--- parkstay/forms.py
+++ parkstay/forms.py
@@ -18,3 +18,9 @@ class MakeBookingsForm(forms.Form):
     price = forms.DecimalField()
     postcode =forms.CharField(max_length=4, label="Post Code")
     country = forms.ChoiceField(label="Country")
+
+    def __init__(self, *args, **kwargs):
+        super(MakeBookingsForm, self).__init__(*args, **kwargs)
+        self.fields['arrival'].widget.attrs['readonly'] = True
+        self.fields['depature'].widget.attrs['readonly'] = True
+        self.fields['guests'].widget.attrs['readonly'] = True
