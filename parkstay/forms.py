0e9f8238067499c1e4151e17028a1e2e5428a6d6
tawanda <tawanda.nyakudjga@gmail.com>
2017-01-11 16:18:20 +0800

    Parkstay: implement makebooking  form validation

--- parkstay/forms.py
+++ parkstay/forms.py
@@ -5,22 +5,23 @@ class LoginForm(forms.Form):
     email = forms.EmailField(max_length=254)
 
 class MakeBookingsForm(forms.Form):
-    arrival = forms.DateField()
-    depature = forms.DateField()
-    guests = forms.CharField()
+    arrival = forms.DateField(widget=forms.TextInput(attrs={'required':True}))
+    depature = forms.DateField(widget=forms.TextInput(attrs={'required':True}))
+    guests = forms.CharField(widget=forms.TextInput(attrs={'required':True}))
     campsite = forms.ChoiceField()
-    email = forms.EmailField()
-    confirmEmail = forms.EmailField(label ="Confirm Email")
+    email = forms.EmailField(widget=forms.TextInput(attrs={'required':True}))
+    confirmEmail = forms.EmailField(label ="Confirm Email",required=False)
     firstName = forms.CharField(label="First Name")
-    surname = forms.CharField()
-    phone = forms.CharField()
-    vehicleRego = forms.CharField(label = "Vehicle Registration")
-    price = forms.DecimalField()
-    postcode =forms.CharField(max_length=4, label="Post Code")
-    country = forms.ChoiceField(label="Country")
+    surname = forms.CharField(widget=forms.TextInput(attrs={'required':True}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'required':True}))
+    vehicleRego = forms.CharField(label = "Vehicle Registration",required=False)
+    price = forms.DecimalField(widget=forms.TextInput(attrs={'required':True}))
+    postcode =forms.CharField(max_length=4, label="Post Code",widget=forms.TextInput(attrs={'required':True}))
+    country = forms.ChoiceField(label="Country",widget=forms.TextInput(attrs={'required':True}))
 
     def __init__(self, *args, **kwargs):
         super(MakeBookingsForm, self).__init__(*args, **kwargs)
         self.fields['arrival'].widget.attrs['readonly'] = True
         self.fields['depature'].widget.attrs['readonly'] = True
         self.fields['guests'].widget.attrs['readonly'] = True
+        self.fields['firstName'].widget.attrs['required'] = True
