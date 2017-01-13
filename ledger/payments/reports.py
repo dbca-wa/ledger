c201f34a62d5662c64b6a99488154819a5838032
Scott Percival <scott.percival@dpaw.wa.gov.au>
2017-01-13 16:30:58 +0800

    ledger.payments.reports.generate_items_csv: overhaul

--- ledger/payments/reports.py
+++ ledger/payments/reports.py
@@ -14,7 +14,15 @@ def generate_items_csv(system,start,end,banked_start,banked_end,region=None,dist
     dates, banked_dates = [], []
     date_amounts, banked_date_amounts = [], []
     items = []
+    oracle_codes = {} 
+    banked_oracle_codes = {}
     date_format = '%A %d/%m/%y'
+
+    eft = []
+    banked_cash = []
+    bpoint = []
+    bpay = []
+
     if invoices:
         strIO = StringIO()
         fieldnames = ['Account Code', 'Day']
@@ -52,10 +60,12 @@ def generate_items_csv(system,start,end,banked_start,banked_end,region=None,dist
         # Dates row
         writer.writerow(['']+ dates_row.split(','))
         writer.writerow([''] + ['Credit Card','Bpay','EFTPOS'] * len(dates) + ['','Credit Card','Bpay','EFTPOS'])
+
         for i in invoices:
             # Add items of invoice if not in list
             if i.order:
                 for x in i.order.lines.all():
+                    #print((i, i.__dict__, x, x.oracle_code))
                     item_date_amounts, banked_item_dates_amounts = [], []
                     for d in dates:
                         item_date_amounts.append({
@@ -78,99 +88,147 @@ def generate_items_csv(system,start,end,banked_start,banked_end,region=None,dist
                                 'money_order': D('0.0')
                             }
                         })
-                    
-                    if not any(it.get('item', None).oracle_code == x.oracle_code for it in items):
-                        items.append({
-                            'dates':item_date_amounts,
-                            'banked_dates': banked_item_dates_amounts,
-                            'item': x,
-                            'card': D('0.0'),
-                            'bpay': D('0.0'),
-                            'eft': D('0.0'),
-                            'cash': D('0.0'),
-                            'cheque': D('0.0'),
-                            'money_order': D('0.0')
-                        })
+
+                    items.append({
+                        'dates':item_date_amounts,
+                        'banked_dates': banked_item_dates_amounts,
+                        'item': x,
+                        'card': D('0.0'),
+                        'bpay': D('0.0'),
+                        'eft': D('0.0'),
+                        'cash': D('0.0'),
+                        'cheque': D('0.0'),
+                        'money_order': D('0.0')
+                    })
+
+                    # create empty subtotal list for each oracle code
+                    if x.oracle_code not in oracle_codes:
+                        oracle_codes[x.oracle_code] = []
+                        for d in daterange(start,end):
+                            oracle_codes[x.oracle_code].append({
+                                'date':d.strftime(date_format),
+                                'amounts':{
+                                    'card': D('0.0'),
+                                    'bpay': D('0.0'),
+                                    'eft': D('0.0'),
+                                    'cash': D('0.0'),
+                                    'cheque': D('0.0'),
+                                    'money_order': D('0.0')
+                                }
+                            })
+
+                    if x.oracle_code not in banked_oracle_codes:
+                        banked_oracle_codes[x.oracle_code] = []
+                        for d in daterange(banked_start,banked_end):
+                            banked_oracle_codes[x.oracle_code].append({
+                                'date':d.strftime(date_format),
+                                'amounts':{
+                                    'cash': D('0.0'),
+                                    'cheque': D('0.0'),
+                                    'money_order': D('0.0')
+                                }
+                            })
+
+        
             # Get all transactions
-            cash = i.cash_transactions.filter(created__gte=start, created__lte=end)
-            banked_cash = i.cash_transactions.filter(created__gte=banked_start, created__lte=banked_end)
-            bpoint = i.bpoint_transactions.filter(created__gte=start, created__lte=end)
-            bpay = i.bpay_transactions.filter(p_date__gte=start, p_date__lte=end)
-            # Go through items
+            eft.extend([x for x in i.cash_transactions.filter(created__gte=start, created__lte=end, source='eft')])
+            banked_cash.extend([x for x in i.cash_transactions.filter(created__gte=banked_start, created__lte=banked_end).exclude(source='eft')])
+            bpoint.extend([x for x in i.bpoint_transactions.filter(created__gte=start, created__lte=end)])
+            bpay.extend([x for x in i.bpay_transactions.filter(p_date__gte=start, p_date__lte=end)])
+        # Go through items
+        #print((start, end))
+        #print([(x, x.created) for x in eft])
+        #print([(x, x.created) for x in banked_cash])
+        #print([(x, x.created) for x in bpoint])
+        #print([(x, x.created) for x in bpay])
+        
+        for item in items:
+            price = D(item.get('item').line_price_before_discounts_incl_tax)
+            code = item.get('item').oracle_code
+            order = item.get('item').order
 
-            for item in items:
-                # Banked Cash
-                for d in item['banked_dates']:
-                    index = 0
-                    for l in banked_date_amounts:
-                        if l.get('date') == d.get('date'):
-                            date_amount_index = index
-                            break
-                        index += 1
-                    for c in banked_cash:
-                        for s in CashTransaction.SOURCE_TYPES:
-                            source = str(s[0])
-                            if c.source == source and source != 'eft':
-                                if c.created.strftime(date_format) == d.get('date'):
-                                    if c.type == 'payment':
-                                        d['amounts'][source] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                        item[source] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                        banked_date_amounts[date_amount_index]['amounts'][source] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                    else:
-                                        d['amounts'][source] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                                        item[source] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                                        banked_date_amounts[date_amount_index]['amounts'][source] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                # Other transactions
-                for d in item['dates']:
-                    index = 0
-                    for l in date_amounts:
-                        if l.get('date') == d.get('date'):
-                            date_amount_index = index
-                            break
-                        index += 1
-                    # EFT
-                    for c in cash:
-                        if c.created.strftime(date_format) == d.get('date'):
-                            if c.type == 'payment'and c.source == 'eft':
-                                d['amounts']['eft'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                item['eft'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                date_amounts[date_amount_index]['amounts']['eft'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                            else:
-                                d['amounts']['eft'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                                item['eft'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                                date_amounts[date_amount_index]['amounts']['eft'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-
-                    # Card
-                    for c in bpoint:
-                        if c.approved:
+            # Banked Cash
+            for d in item['banked_dates']:
+                index = 0
+                for l in banked_date_amounts:
+                    if l.get('date') == d.get('date'):
+                        date_amount_index = index
+                        break
+                    index += 1
+                for c in banked_cash:
+                    for s in CashTransaction.SOURCE_TYPES:
+                        source = str(s[0])
+                        if c.source == source and c.invoice.order == order:
                             if c.created.strftime(date_format) == d.get('date'):
-                                if c.action == 'payment' or c.action == 'capture':
-                                    d['amounts']['card'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                    item['card'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                    date_amounts[date_amount_index]['amounts']['card'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                elif c.action == 'reversal' or c.action == 'refund':
-                                    d['amounts']['card'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                                    item['card'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                                    date_amounts[date_amount_index]['amounts']['card'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                    # BPAY
-                    for b in bpay:
-                        if b.approved:
-                            if b.p_date.strftime(date_format) == d.get('date'):
-                                if b.p_instruction_code == '05' and b.type == '399':
-                                    d['amounts']['bpay'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                    item['bpay'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                    date_amounts[date_amount_index]['amounts']['bpay'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                elif b.p_instruction_code == '25' and b.type == '699':
-                                    d['amounts']['bpay'] += D(item.get('item').line_price_before_discounts_incl_tax)
-                                    item['bpay'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-                                    date_amounts[date_amount_index]['amounts']['bpay'] -= D(item.get('item').line_price_before_discounts_incl_tax)
-        for i in items:
+                                if c.type == 'payment':
+                                    banked_oracle_codes[code][date_amount_index]['amounts'][source] += price
+                                    item[source] += price 
+                                    banked_date_amounts[date_amount_index]['amounts'][source] += price
+                                elif c.type in ('refund', 'reversal'):
+                                    banked_oracle_codes[code][date_amount_index]['amounts'][source] -= price
+                                    item[source] -= price
+                                    banked_date_amounts[date_amount_index]['amounts'][source] -= price
+            # Other transactions
+            for d in oracle_codes[code]:
+                index = 0
+                for l in date_amounts:
+                    if l.get('date') == d.get('date'):
+                        date_amount_index = index
+                        break
+                    index += 1
+                # EFT
+                for c in eft:
+                    if c.created.strftime(date_format) == d.get('date') and c.invoice.order == order:
+                        if c.type == 'payment':
+                            oracle_codes[code][date_amount_index]['amounts']['eft'] += price
+                            item['eft'] += price
+                            date_amounts[date_amount_index]['amounts']['eft'] += price
+                        elif c.type in ('refund', 'reversal'):
+                            oracle_codes[code][date_amount_index]['amounts']['eft'] -= price
+                            item['eft'] -= price
+                            date_amounts[date_amount_index]['amounts']['eft'] -= price
+
+                # Card
+                for c in bpoint:
+                    if c.approved and c.order == order:
+                        if c.created.strftime(date_format) == d.get('date'):
+                            #print(('CC', price, code, c, c.__dict__, d))
+                            if c.action in ('payment', 'capture'):
+                                oracle_codes[code][date_amount_index]['amounts']['card'] += price
+                                item['card'] += price
+                                date_amounts[date_amount_index]['amounts']['card'] += price
+                            elif c.action in ('refund', 'reversal'):
+                                oracle_codes[code][date_amount_index]['amounts']['card'] -= price
+                                item['card'] -= price
+                                date_amounts[date_amount_index]['amounts']['card'] -= price
+                # BPAY
+                for b in bpay:
+                    if b.approved and b.order == order:
+                        if b.p_date.strftime(date_format) == d.get('date'):
+                            if b.p_instruction_code == '05' and b.type == '399':
+                                oracle_codes[code][date_amount_index]['amounts']['bpay'] += price
+                                item['bpay'] += price
+                                date_amounts[date_amount_index]['amounts']['bpay'] += price
+                            elif b.p_instruction_code == '25' and b.type == '699':
+                                oracle_codes[code][date_amount_index]['amounts']['bpay'] -= price
+                                item['bpay'] -= price
+                                date_amounts[date_amount_index]['amounts']['bpay'] -= price
+   
+
+        for code in oracle_codes:
             item_str = ''
-            item_str += '{},'.format(str(i.get('item').oracle_code))
-            for d in i['dates']:
+            item_str += '{},'.format(code)
+            card_total = D('0.0')
+            bpay_total = D('0.0')
+            eft_total = D('0.0')
+            for d in oracle_codes[code]:
                 item_str += '{},{},{},'.format(d['amounts']['card'],d['amounts']['bpay'],d['amounts']['eft'])
-            item_str += ',{},{},{},'.format(i['card'],i['bpay'],i['eft'])
-            writer.writerow(item_str.split(','))
+                card_total += d['amounts']['card']
+                bpay_total += d['amounts']['bpay']
+                eft_total += d['amounts']['eft']
+            item_str += ',{},{},{},'.format(card_total, bpay_total, eft_total)
+            if not ((card_total == D('0.0')) and (bpay_total == D('0.0')) and (eft_total == D('0.0'))):
+                writer.writerow(item_str.split(','))
 
         total_str = 'Totals,'
         total_amounts = {
@@ -196,13 +254,20 @@ def generate_items_csv(system,start,end,banked_start,banked_end,region=None,dist
         writer.writerow(['']+ banked_dates_row.split(','))
         writer.writerow([''] + ['Cash','Cheque','Money Order'] * len(banked_dates) + ['','Cash','Cheque','Money Order','Banked(Cash,Money Order,Cheque)'])
 
-        for i in items:
+        for code in banked_oracle_codes:
             banked_item_str = ''
-            banked_item_str += '{},'.format(str(i.get('item').oracle_code))
-            for d in i['banked_dates']:
+            banked_item_str += '{},'.format(code)
+            cash_total = D('0.0')
+            cheque_total = D('0.0')
+            moneyorder_total = D('0.0')
+            for d in banked_oracle_codes[code]:
                 banked_item_str += '{},{},{},'.format(d['amounts']['cash'],d['amounts']['cheque'],d['amounts']['money_order'])
-            banked_item_str += ',{},{},{},'.format(i['cash'],i['cheque'],i['money_order'])
-            writer.writerow(banked_item_str.split(','))
+                cash_total += d['amounts']['cash']
+                cheque_total += d['amounts']['cheque']
+                moneyorder_total += d['amounts']['money_order']
+            banked_item_str += ',{},{},{},'.format(cash_total, cheque_total, moneyorder_total)
+            if not ((cash_total == D('0.0')) and (cheque_total == D('0.0')) and (moneyorder_total == D('0.0'))):
+                writer.writerow(banked_item_str.split(','))
 
         banked_total_str = 'Totals,'
         banked_total_amounts = {
