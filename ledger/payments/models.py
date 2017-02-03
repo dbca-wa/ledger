7337d14acf6eb955f29b9ba5ee07cde0afd707d5
Brian <ndwigabrian@gmail.com>
2017-02-03 14:47:18 +0800

    ledger: added bpay notification models

--- ledger/payments/models.py
+++ ledger/payments/models.py
@@ -1,4 +1,4 @@
-from ledger.payments.bpay.models import BpayTransaction, BpayFile
+from ledger.payments.bpay.models import BpayTransaction, BpayFile, BillerCodeRecipient, BillerCodeSystem
 from ledger.payments.invoice.models import Invoice, InvoiceBPAY
 from ledger.payments.bpoint.models import BpointTransaction, BpointToken
 from ledger.payments.cash.models import CashTransaction
