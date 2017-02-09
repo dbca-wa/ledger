adcaa67a6e637e2588928805aa197a6a8e778424
Scott Percival <scott.percival@dpaw.wa.gov.au>
2017-02-09 20:39:52 +0800

    parkstay.utils: add availability checker

--- parkstay/utils.py
+++ parkstay/utils.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta
 
+from decimal import *
+
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import transaction
+from django.db.models import Q    
 from django.utils import timezone
 
-from parkstay.models import (Campground, Campsite, CampsiteBooking, Booking)
+from parkstay.models import (Campground, Campsite, CampsiteBooking, Booking, CampsiteBookingRange, CampgroundBookingRange)
 
 
 def create_booking_by_class(campground_id, campsite_class_id, start_date, end_date, num_adult=0, num_concession=0, num_child=0, num_infant=0):
@@ -111,3 +114,48 @@ def create_booking_by_site(campsite_id, start_date, end_date, num_adult=0, num_c
     # On success, return the temporary booking
     return booking
 
+
+def get_campsite_availability(campsites_qs, start_date, end_date):
+    # fetch all of the single-day CampsiteBooking objects within the date range for the sites
+    bookings_qs =   CampsiteBooking.objects.filter(
+                        campsite__in=campsites_qs,
+                        date__gte=start_date,
+                        date__lt=end_date
+                    ).order_by('date', 'campsite__name')
+    
+    duration = (end_date-start_date).days
+    results = {site.pk: {start_date+timedelta(days=i): ['open', ] for i in range(duration)} for site in campsites_qs}
+
+    # strike out existing bookings
+    for b in bookings_qs:
+        results[b.campsite.pk][b.date][0] = 'closed' if b.booking_type == 2 else 'booked'
+    
+    # generate a campground-to-campsite-list map
+    campground_map = {cg[0]: [cs.pk for cs in campsites_qs if cs.campground.pk == cg[0]] for cg in campsites_qs.distinct('campground').values_list('campground')}
+
+    # strike out whole campground closures
+    cgbr_qs =    CampgroundBookingRange.objects.filter(
+        Q(campground__in=campground_map.keys()),
+        Q(status=1),
+        Q(range_start__lt=end_date) & Q(range_end__gte=start_date)
+    )
+    for closure in cgbr_qs:
+        start = max(start_date, closure.range_start)
+        end = min(end_date, closure.range_end)
+        for i in range((end-start).days):
+            for cs in campground_map[closure.campground.pk]:
+                results[cs][start+timedelta(days=i)][0] = 'closed'
+
+    # strike out campsite closures
+    csbr_qs =    CampsiteBookingRange.objects.filter(
+        Q(campsite__in=campsites_qs),
+        Q(status=1),
+        Q(range_start__lt=end_date) & Q(range_end__gte=start_date)
+    )
+    for closure in csbr_qs:
+        start = max(start_date, closure.range_start)
+        end = min(end_date, closure.range_end)
+        for i in range((end-start).days):
+            results[closure.campsite.pk][start+timedelta(days=i)][0] = 'closed'
+
+    return results
