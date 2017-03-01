c9910c848dc832ee33ff945a74d304081184a3a8
Scott Percival <scott.percival@dpaw.wa.gov.au>
2017-03-01 15:06:13 +0800

    parkstay.frontend.availability: init

---
+++ parkstay/frontend/availability/src/main.js
@@ -0,0 +1,12 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'foundation-sites/dist/css/foundation-flex.css';
+import 'foundation-datepicker/css/foundation-datepicker.css';
+
+import Vue from 'vue';
+import availability from './availability';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue(availability);
