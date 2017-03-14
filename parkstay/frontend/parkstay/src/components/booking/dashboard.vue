c2f0a3e36f4917e9bb5317b9c0ef5a16974b40d0
Brian Ndwiga <ndwigabrian@gmail.com>
2017-03-14 15:04:12 +0800

    parkstay: link booking record payment button to the manual payments screen

--- parkstay/frontend/parkstay/src/components/booking/dashboard.vue
+++ parkstay/frontend/parkstay/src/components/booking/dashboard.vue
@@ -222,8 +222,13 @@ export default {
                             var invoice = "/ledger/payments/invoice/"+full.invoice_reference;
                             var invoice_link= (full.invoice_reference)?"<a href='"+invoice+"' target='_blank' class='text-primary'>Invoice</a><br/>":"";
                             var column = "<td >";
+                            var invoice_string = '/ledger/payments/invoice/payment?';
+                            $.each(full.invoices,function(i,n){
+                                invoice_string += 'invoice='+n+'&';
+                            });
+                            invoice_string.trim('&');
                             if (!full.paid){
-                                var record_payment = "<a href='#' class='text-primary' data-rec-payment='' > Record Payment</a><br/>";
+                                var record_payment = "<a href='"+invoice_string+"' target='_blank' class='text-primary' data-rec-payment='' > Record Payment</a><br/>";
                                 column += record_payment;
                             }
                             if (full.editable){
