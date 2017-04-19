27f4767f4a6808c18444147407ef563d257fbb99
Brian Ndwiga <ndwigabrian@gmail.com>
2017-04-19 09:56:48 +0800

    ledger: add payments details for individual line items

--- ledger/order/models.py
+++ ledger/order/models.py
@@ -1,13 +1,39 @@
+import json
+from decimal import Decimal as D
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.postgres.fields import JSONField
 from oscar.apps.order.abstract_models import AbstractLine as CoreAbstractLine
 
 class Line(CoreAbstractLine):
 
+    DEFAULT_PAYMENT = {
+        'bpay': '{}',
+        'cash': '{}',
+        'card': '{}'
+    }
     oracle_code = models.CharField("Oracle Code",max_length=50,null=True,blank=True)
     partner_name = models.CharField(
         _("Partner name"), max_length=128, blank=True,null=True)
     partner_sku = models.CharField(_("Partner SKU"), max_length=128,null=True)
+    payment_details = JSONField(default=json.dumps(DEFAULT_PAYMENT))
+    refund_details = JSONField(default=json.dumps(DEFAULT_PAYMENT))
+
+    @property
+    def paid(self):
+        amount = D(0.0)
+        for k,v in self.payment_details.items():
+            for i,a in v.items():
+                amount += D(a)
+        return amount
+
+    @property
+    def refunded(self):
+        amount = D(0.0)
+        for k,v in self.refund_details.items():
+            for i,a in v.items():
+                amount += D(a)
+        return amount
 
     # A line reference is the ID that a partner uses to represent this
     # particular line (it's not the same as a SKU).
