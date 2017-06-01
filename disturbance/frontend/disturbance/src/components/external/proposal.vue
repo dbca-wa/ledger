314d3b8360095ef53d1683c439371aef348c9d94
tawanda <tawanda.nyakudjga@gmail.com>
2017-06-01 10:04:54 +0800

    form handling

--- disturbance/frontend/disturbance/src/components/external/proposal.vue
+++ disturbance/frontend/disturbance/src/components/external/proposal.vue
@@ -1,7 +1,8 @@
 <template lang="html">
     <div >
-        <form action="/" method="post">
+        <form action="/" method="post" name="new_proposal">
             <Proposal :proposal="proposal" />
+            <input type="button" @click.prevent.stop="submit">
         </form>
     </div>
 </template>
@@ -17,6 +18,14 @@ export default {
     },
     components:{
         Proposal
+    },
+    methods:{
+        submit:function (e) {
+            console.log($(form).serializeArray());
+        }
+    },
+    mounted:function(){
+        var form = document.forms.new_proposal;
     }
 }
 </script>
