3954cc8b6771cf85da63f68796d98682c0d463e9
Tony Prior <tony@gaiaresources.com.au>
2017-06-27 15:26:42 +0800

    Fixed data URL to handle return_type_pk and resource_number greater than single digit.

--- wildlifelicensing/apps/returns/api/urls.py
+++ wildlifelicensing/apps/returns/api/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from wildlifelicensing.apps.returns.api import views
 
 urlpatterns = [
-    url(r'data/(?P<return_type_pk>[0-9])/(?P<resource_number>[0-9]?)/?', views.ReturnsDataView.as_view(),
+    url(r'data/(?P<return_type_pk>[0-9]+)/(?P<resource_number>[0-9]+)/?', views.ReturnsDataView.as_view(),
         name='data'),
     url(r'', views.ExplorerView.as_view(), name='explorer')
 ]
