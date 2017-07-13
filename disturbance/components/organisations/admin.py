ce640f63a2073d7a2598a6b4565edd3c79f797ab
Brian Ndwiga <ndwigabrian@gmail.com>
2017-07-13 09:42:16 +0800

    disturbance: fix the filter for members in the organisation access group

--- disturbance/components/organisations/admin.py
+++ disturbance/components/organisations/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from ledger.accounts.models import EmailUser
 from disturbance.components.organisations import models
 # Register your models here.
 
@@ -12,8 +13,13 @@ class OrganisationRequestAdmin(admin.ModelAdmin):
 
 @admin.register(models.OrganisationAccessGroup)
 class OrganisationAccessGroupAdmin(admin.ModelAdmin):
-
+    filter_horizontal = ('members',)
     exclude = ('site',)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "members":
+            kwargs["queryset"] = EmailUser.objects.filter(email__icontains='@dbca.wa.gov.au')
+        return super(OrganisationAccessGroupAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
     def has_add_permission(self, request):
         return True if models.OrganisationAccessGroup.objects.count() == 0 else False
