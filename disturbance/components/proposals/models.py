97c4022264006e1206b67029c58a80aa3a7faaa1
Brian Ndwiga <ndwigabrian@gmail.com>
2017-07-24 10:28:25 +0800

    disturbance: check if referral is in ledger before checking the cms

--- disturbance/components/proposals/models.py
+++ disturbance/components/proposals/models.py
@@ -304,17 +304,22 @@ class Proposal(RevisionedMixin):
                     self.processing_status = 'with_referral'
                     self.save()
                     referral = None
-                    # Validate if it is a deparment user
-                    department_user = get_department_user(referral_email)
-                    if not department_user:
-                        raise ValidationError('The user you want to send the referral to is not a member of the department')
-                    # Check if the user is in ledger or create
-                    
-                    user,created = EmailUser.objects.get_or_create(email=department_user['email'].lower())
-                    if created:
-                        user.first_name = department_user['given_name']
-                        user.last_name = department_user['surname']
-                        user.save()
+
+                    # Check if the user is in ledger
+                    try:
+                        user = EmailUser.objects.get(email__icontains=referral_email)
+                    except EmailUser.DoesNotExist:
+                        # Validate if it is a deparment user
+                        department_user = get_department_user(referral_email)
+                        if not department_user:
+                            raise ValidationError('The user you want to send the referral to is not a member of the department')
+                        # Check if the user is in ledger or create
+                        
+                        user,created = EmailUser.objects.get_or_create(email=department_user['email'].lower())
+                        if created:
+                            user.first_name = department_user['given_name']
+                            user.last_name = department_user['surname']
+                            user.save()
                     try:
                         Referral.objects.get(referral=user,proposal=self)
                         raise ValidationError('A referral has already been sent to this user')
@@ -656,17 +661,21 @@ class Referral(models.Model):
                     self.proposal.processing_status = 'with_referral'
                     self.proposal.save()
                     referral = None
-                    # Validate if it is a deparment user
-                    department_user = get_department_user(referral_email)
-                    if not department_user:
-                        raise ValidationError('The user you want to send the referral to is not a member of the department')
-                    # Check if the user is in ledger or create
-                    
-                    user,created = EmailUser.objects.get_or_create(email=department_user['email'].lower())
-                    if created:
-                        user.first_name = department_user['given_name']
-                        user.last_name = department_user['surname']
-                        user.save()
+                    # Check if the user is in ledger
+                    try:
+                        user = EmailUser.objects.get(email__icontains=referral_email)
+                    except EmailUser.DoesNotExist:
+                        # Validate if it is a deparment user
+                        department_user = get_department_user(referral_email)
+                        if not department_user:
+                            raise ValidationError('The user you want to send the referral to is not a member of the department')
+                        # Check if the user is in ledger or create
+                        
+                        user,created = EmailUser.objects.get_or_create(email=department_user['email'].lower())
+                        if created:
+                            user.first_name = department_user['given_name']
+                            user.last_name = department_user['surname']
+                            user.save()
                     try:
                         Referral.objects.get(referral=user,proposal=self.proposal)
                         raise ValidationError('A referral has already been sent to this user')
