b2b90997079245ef6c3238a7228cbfb84c193d67
Brian Ndwiga <ndwigabrian@gmail.com>
2017-08-01 11:06:35 +0800

    disturbance: try and fix width of table

--- disturbance/frontend/disturbance/src/components/common/proposals_dashboard.vue
+++ disturbance/frontend/disturbance/src/components/common/proposals_dashboard.vue
@@ -147,7 +147,41 @@ export default {
                             return 'P'+data;
                         }
                     },
-                    {data: "region"},
+                    {
+                        data: "region",
+                        'render': function (value) {
+                            var ellipsis = '...',
+                                truncated = _.truncate(value, {
+                                    length: 30,
+                                    omission: ellipsis,
+                                    separator: ' '
+                                }),
+                                result = '<span>' + truncated + '</span>',
+                                popTemplate = _.template('<a href="#" ' +
+                                    'role="button" ' +
+                                    'data-toggle="popover" ' +
+                                    'data-trigger="click" ' +
+                                    'data-placement="top auto"' +
+                                    'data-html="true" ' +
+                                    'data-content="<%= text %>" ' +
+                                    '>more</a>');
+                            if (_.endsWith(truncated, ellipsis)) {
+                                result += popTemplate({
+                                    text: value
+                                });
+                            }
+
+                            return result;
+                        },
+                        'createdCell': function (cell) {
+                            $(cell).find('[data-toggle="popover"]')
+                                .popover()
+                                .on('click', function (e) {
+                                    e.preventDefault();
+                                    return true;
+                                });
+                        }
+                    },
                     {data: "activity"},
                     {
                         data: "title",
@@ -293,7 +327,41 @@ export default {
                             return 'P'+data;
                         }
                     },
-                    {data: "region"},
+                    {
+                        data: "region",
+                        'render': function (value) {
+                            var ellipsis = '...',
+                                truncated = _.truncate(value, {
+                                    length: 30,
+                                    omission: ellipsis,
+                                    separator: ' '
+                                }),
+                                result = '<span>' + truncated + '</span>',
+                                popTemplate = _.template('<a href="#" ' +
+                                    'role="button" ' +
+                                    'data-toggle="popover" ' +
+                                    'data-trigger="click" ' +
+                                    'data-placement="top auto"' +
+                                    'data-html="true" ' +
+                                    'data-content="<%= text %>" ' +
+                                    '>more</a>');
+                            if (_.endsWith(truncated, ellipsis)) {
+                                result += popTemplate({
+                                    text: value
+                                });
+                            }
+
+                            return result;
+                        },
+                        'createdCell': function (cell) {
+                            $(cell).find('[data-toggle="popover"]')
+                                .popover()
+                                .on('click', function (e) {
+                                    e.preventDefault();
+                                    return true;
+                                });
+                        }
+                    },
                     {data: "activity"},
                     {
                         data: "title",
