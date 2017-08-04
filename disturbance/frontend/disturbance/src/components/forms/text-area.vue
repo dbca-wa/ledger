4e365acba617659c5033c0148de89e9a04a50662
Brian Ndwiga <ndwigabrian@gmail.com>
2017-08-04 15:08:30 +0800

    disturbance: forgot to add icon in text area

--- disturbance/frontend/disturbance/src/components/forms/text-area.vue
+++ disturbance/frontend/disturbance/src/components/forms/text-area.vue
@@ -4,6 +4,10 @@
             <label for="label" >{{ label }}</label>
             <i data-toggle="tooltip" v-if="help_text" data-placement="right" class="fa fa-question-circle" style="color:blue" :title="help_text">&nbsp;</i>
             <i data-toggle="tooltip" v-if="help_text_assessor && assessorMode" data-placement="right" class="fa fa-question-circle" style="color:green" :title="help_text_assessor">&nbsp;</i>
+            <template v-if="assessorMode">
+                <a href="" v-if="!showingComment" @click.prevent="toggleComment"><i class="fa fa-comment-o">&nbsp;</i></a>
+                <a href="" v-else  @click.prevent="toggleComment"><i class="fa fa-ban">&nbsp;</i></a>
+            </template>
             <textarea :readonly="readonly" class="form-control" rows="5" :name="name">{{ value }}</textarea><br/>
         </div>
         <Comment :readonly="assessor_readonly" :name="name+'-comment-field'" v-show="showingComment && assessorMode" :value="comment_value"/> 
