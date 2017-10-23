6af46e12e6001e0da4a4b775cfe53def1d7304f5
Scott Percival <scott.percival@dbca.wa.gov.au>
2017-10-23 09:24:49 +0800

    parkstay.frontend: last minute JS changes

--- parkstay/frontend/availability/config/dev.env.js
+++ parkstay/frontend/availability/config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  PARKSTAY_URL: '"https://parkstay-rc.dbca.wa.gov.au"'
+  PARKSTAY_URL: '"https://parkstaybookings.dbca.wa.gov.au"'
 })
