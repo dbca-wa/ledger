a9d620b4d3ae21850064c5d9a56ab595f01d1743
Brian Ndwiga <ndwigabrian@gmail.com>
2017-10-30 12:37:13 +0800

    ps: fix an issue when first time user tries to make a booking

--- parkstay/views.py
+++ parkstay/views.py
@@ -267,7 +267,7 @@ class MakeBookingsView(TemplateView):
                         phone_number=form.cleaned_data.get('phone'),
                         mobile_number=form.cleaned_data.get('phone')
                 )
-                Address.objects.create(line1='address', user=customer, postcode=form.cleaned_data.get('postcode'), country=form.cleaned_data.get('country'))
+                Address.objects.create(line1='address', user=customer, postcode=form.cleaned_data.get('postcode'), country=form.cleaned_data.get('country').iso_3166_1_a2)
         else:
             customer = request.user
         
