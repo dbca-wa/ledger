d3d1c41bc34d5ba988bddeab60cd6eff83786bfe
Brian Ndwiga <ndwigabrian@gmail.com>
2017-11-17 12:26:48 +0800

    ps: fix the date for the cancelation time in the export to csv

--- parkstay/frontend/parkstay/src/components/booking/dashboard.vue
+++ parkstay/frontend/parkstay/src/components/booking/dashboard.vue
@@ -540,7 +540,7 @@ export default {
                                 bk[field] = booking.cancelation_reason;
                             break;
                             case 20:
-                                bk[field] = booking.cancelation_time;
+                                bk[field] = Moment(booking.cancelation_time).format("DD/MM/YYYY HH:MM:SS");
                             break;
                             case 21:
                                 bk[field] = booking.canceled_by;
