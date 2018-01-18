a3101b5153a826313f5f265f57d1892b8bd7d658
Ashley Felton <ashley@ropable.com>
2018-01-18 14:24:55 +0800

    Added compiled disturbance frontend static files.

--- disturbance/admin.py
+++ disturbance/admin.py
@@ -3,6 +3,6 @@ from django.contrib.admin import AdminSite
 
 class DisturbanceAdminSite(AdminSite):
     site_header = 'Disturbance Administration'
-    site_title = 'Diturbance Licensing'
+    site_title = 'Disturbance Licensing'
 
 disturbance_admin_site = DisturbanceAdminSite(name='disturbanceadmin')
