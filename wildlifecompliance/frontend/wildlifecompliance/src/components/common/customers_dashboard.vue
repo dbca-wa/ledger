796aa45ed6d0f97ac73d9147f6034c7f155eeba3
wilsonc86 <wilson.cheung86@gmail.com>
2018-02-27 10:52:49 +0800

    WildlifeCompliance: customer WIP

---
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/common/customers_dashboard.vue
@@ -0,0 +1,122 @@
+<template id="customer_dashboard">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Customers <small v-if="is_external">View customer details</small>
+                        <a :href="'#'+pBody" data-toggle="collapse"  data-parent="#userInfo" expanded="true" :aria-controls="pBody">
+                            <span class="glyphicon glyphicon-chevron-up pull-right "></span>
+                        </a>
+                    </h3>
+                </div>
+                <div class="panel-body collapse in" :id="pBody">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <datatable ref="customer_datatable" :id="datatable_id" :dtOptions="customer_options" :dtHeaders="customer_headers"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import datatable from '@/utils/vue/datatable.vue'
+import {
+    api_endpoints,
+    helpers
+}from '@/utils/hooks'
+export default {
+    name: 'CustomerTableDash',
+    props: {
+        level:{
+            type: String,
+            required: true,
+            validator:function(val) {
+                let options = ['internal','referral'];
+                return options.indexOf(val) != -1 ? true: false;
+            }
+        },
+        url:{
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        let vm = this;
+        return {
+            pBody: 'pBody' + vm._uid,
+            datatable_id: 'customer-datatable-'+vm._uid,
+            // Filters for Customers 
+            dateFormat: 'DD/MM/YYYY',
+            datepickerOptions:{
+                format: 'DD/MM/YYYY',
+                showClear:true,
+                useCurrent:false,
+                keepInvalid:true,
+                allowInputToggle:true
+            },
+            customer_headers:["Title","Given Name(s)","Last Name","Date of Birth","Email","Phone","Mobile","Fax","Character Check","Identification"],
+            customer_options:{
+                language: {
+                    processing: "<i class='fa fa-4x fa-spinner fa-spin'></i>"
+                },
+                responsive: true,
+                ajax: {
+                    "url": vm.url,
+                    "dataSrc": ''
+                },
+                columns: [
+                    {data: "id"},
+                    {data: "title"},
+                    {data: "first_name"},
+                    {data: "last_name"},
+                    {
+                        data: "dob",
+                        mRender:function (data,type,full) {
+                            return data != '' && data != null ? moment(data).format(vm.dateFormat): '';
+                        }
+                    },
+                    {data: "email"},
+                    {data: "phone"},
+                    {data: "mobile"},
+                    {data: "fax"},
+                    {data: "character_flagged"},
+                    {data: "identification"},
+                ],
+                processing: true,
+            }
+        }
+    },
+    components:{
+        datatable
+    },
+    computed: {
+        status: function(){
+            //return this.is_external ? this.external_status : this.internal_status;
+            return [];
+        },
+        is_external: function(){
+            return this.level == 'external';
+        },
+        is_referral: function(){
+            return this.level == 'referral';
+        }
+    },
+    mounted: function(){
+        let vm = this;
+        $( 'a[data-toggle="collapse"]' ).on( 'click', function () {
+            var chev = $( this ).children()[ 0 ];
+            window.setTimeout( function () {
+                $( chev ).toggleClass( "glyphicon-chevron-down glyphicon-chevron-up" );
+            }, 100 );
+        });
+        this.$nextTick(() => {
+            vm.addEventListeners();
+            vm.initialiseSearch();
+        });
+    }
+}
+</script>
+<style scoped>
+</style>
---
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/internal/customers/dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container" id="internalDash">
+    <CustomerDashTable level="internal" :url="customers_url"/>
+</div>
+</template>
+<script>
+import CustomerDashTable from '@common-utils/customers_dashboard.vue'
+import {
+  api_endpoints,
+  helpers
+}
+from '@/utils/hooks'
+export default {
+    name: 'InternalCustomerDashboard',
+    data() {
+        let vm = this;
+        return {
+            customers_url: api_endpoints.customers,
+        }
+    
+    },
+    watch: {},
+    components: {
+        CustomerDashTable,
+    },
+    computed: {
+    },
+    methods: {},
+    mounted: function () {
+    }
+}
+</script>
