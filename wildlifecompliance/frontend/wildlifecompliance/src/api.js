8357ef94ace7b1c447e94be9c55a986ed882b6ba
Wilson Cheung <wilson.cheung86@gmail.com>
2018-04-17 13:04:04 +0800

    WildlifeCompliance: #5124 account/profile WIP

--- wildlifecompliance/frontend/wildlifecompliance/src/api.js
+++ wildlifecompliance/frontend/wildlifecompliance/src/api.js
@@ -3,7 +3,7 @@ module.exports = {
     organisation_requests: '/api/organisation_requests.json',
     organisation_contacts: '/api/organisation_contacts.json',
     organisation_access_group_members: '/api/organisation_access_group_members',
-    users: '/api/users.json',
+    users: '/api/users',
     profile: '/api/profile',
     emailidentities: '/api/emailidentities',
     profiles: '/api/profiles',
