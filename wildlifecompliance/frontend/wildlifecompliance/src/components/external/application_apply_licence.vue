884929bb119adcf4787556d8f86d0c57c999736a
Indrayani Jadhav <indrayanijadhav@aws-mate-003.aws>
2018-05-01 14:25:08 +0800

    Wildlifecompliance:4883 Radio button and check box selection of licence class,activitytypes

--- wildlifecompliance/frontend/wildlifecompliance/src/components/external/application_apply_licence.vue
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/external/application_apply_licence.vue
@@ -24,7 +24,7 @@
                                 <div v-for="(category,index) in licence_categories" class="radio">
                                     <div class ="row">
                                         <div class="col-sm-9" >  
-                                            <input type="radio"  :id="category.id" name="licence_category" v-model="licence_category.id"  :value="category.id" @change="handleChange($event,index)"> {{category.name}} {{category.checked}}
+                                            <input type="radio"  :id="category.id" name="licence_category" v-model="licence_categories.id"  :value="category.id" @change="handleRadioChange($event,index)"> {{category.name}} 
                                              
                                             <div class="row">
 
@@ -32,15 +32,17 @@
                                                 <div  v-if="category.checked" class="col-sm-9"> 
 
                                                     <div v-for="(type,index1) in category.activity_type" class="checkbox">
-                                                        <input type="checkbox" ref="selected_activity_type" name ="activity_type" :value="type.id" :id = "type.id" v-model="category.activity_type[index1].selected" @click="handleCheckboxChange(index,index1)"> {{type.name}} 
+                                                        <input type="checkbox" ref="selected_activity_type" name ="activity_type" :value="type.id" :id = "type.id" v-model="category.activity_type[index1].selected" @click="handleActivityTypeCheckboxChange(index,index1)"> {{type.name}} 
                                                         
-                                                        <!-- <div v-for="activity in type.activity" class="checkbox">
-                                                            
-                                                            <div class ="col-sm-6">
-                                                                <input type="checkbox" :value="activity.id" :id="activity.id" v-model="selected_activity">{{activity.name}}
-                                                            </div>
+                                                        <div v-if="type.selected">
+                                                            <div v-for="(activity,index2) in type.activity" class="checkbox">
+                                                                
+                                                                <div class ="col-sm-6">
+                                                                    <input type="checkbox" :value="activity.id" :id="activity.id" v-model="type.activity[index2].selected"@click="handleActivityCheckboxChange()" >{{activity.name}}
+                                                                </div>
 
-                                                        </div> -->
+                                                            </div>
+                                                        </div>
                                                         
                                                     </div>
                                                 </div> 
@@ -87,14 +89,15 @@ export default {
         },
         
         radio_selected : [],
-        selected_activity_type: [],
+        // selected_activity_type: [],
         selected_activity:[],
         activity_type_showing : [],
         organisations:null,
         licence_categories : {
             checked:false,
             activity_type:[
-             { id:null,
+             { 
+             	id:null,
                 selected:false,
                 activity:[]
              }
@@ -108,10 +111,10 @@ export default {
   components: {
   },
   watch:{
-    selected_activity_type: function(){
-        this.activity_type.id = this.selected_activity_type;
+    // selected_activity_type: function(){
+    //     this.activity_type.id = this.selected_activity_type;
 
-    }
+    // }
 
   },
   computed: {
@@ -142,34 +145,39 @@ export default {
         });
     },
 
-    handleChange: function(e,index){
+    handleRadioChange: function(e,index){
         let vm=this
-        
-        console.log("inside handle change")
-        for(var i=0,_len=this.licence_categories.length;i<_len;i++){
+        for(var i=0,_len=vm.licence_categories.length;i<_len;i++){
             if(i===index){
-                this.licence_categories[i].checked = true;
+                vm.licence_categories[i].checked = true;
             }else{
-                this.licence_categories[i].checked=false;
+                for(var activity_type_index=0, len1=vm.licence_categories[i].activity_type.length; activity_type_index<len1; activity_type_index++){
+                        if (vm.licence_categories[i].activity_type[activity_type_index].selected){
+                            vm.licence_categories[i].activity_type[activity_type_index].selected=false;
+                            for(var activity_index=0, len2=vm.licence_categories[i].activity_type[activity_type_index].activity.length; activity_index<len2; activity_index++){
+
+                            }    
+                        }
+                }
+                vm.licence_categories[i].checked=false;
             }
 
         }
-        // vm.radio_selected[index] = !vm.radio_selected[index]
 
     },
-    handleCheckboxChange:function(index,index1){
+    handleActivityTypeCheckboxChange:function(index,index1){
         let vm = this
-        console.log("licence category",vm.licence_category)
         var input = $(vm.$refs.selected_activity_type)[0];
-        // vm.licence_categories.activity_type[index1].id=input.id;
-
-        console.log("Input ",input.selected)
-        // vm.licence_categories[index].activity_type[index1].id = 
+        if(!vm.licence_categories[index].activity_type[index1].selected){
+            for(var activity_index=0, len2=vm.licence_categories[index].activity_type[index1].activity.length; activity_index<len2; activity_index++){
+                         vm.licence_categories[index].activity_type[index1].activity[activity_index].selected= false;
+                            }    
+        }
     },
     createApplication:function () {
         let vm = this;
-        let select_type = JSON.stringify(vm.selected_activity_type)
-        console.log("from areate application",select_type)
+        let category = JSON.stringify(vm.licence_categories)
+        console.log("from areate application",category)
         vm.$http.post('/api/application.json',{
             behalf_of: vm.behalf_of,
             licence_activity_type:vm.selected_activity_type,
@@ -188,12 +196,21 @@ export default {
     },
     
   },
+ 
+  created:function(){
+    console.log("created application apply licence")
+  },
+  beforeMount:function(){
+    console.log("Before mpunt application apply licence")
+  },
    
   mounted: function() {
     let vm = this;
+    console.log("1inside mounted")
     vm.form = document.forms.new_application;
     console.log(vm.licence_select);
     console.log("Mounted",this.licence_select)
+    console.log("2. Inside mounted end")
 
   },
   beforeRouteEnter:function(to,from,next){
