6b7958ff3f1ba9012e0db4a1a8381dbf0b2263c5
Fatemeh Moghadam <fatemeh.moghaddam@dbca.wa.gov.au>
2018-06-01 12:51:06 +0800

    select all checkbox and select reason issue fixed

--- parkstay/frontend/parkstay/src/components/booking/addbooking.vue
+++ parkstay/frontend/parkstay/src/components/booking/addbooking.vue
@@ -80,7 +80,9 @@
                                         <thead>
                                             <tr>
                                                 <th class="form-group">Campsite</th>
-                                                <th >Sites to book</th>
+                                                <th >Sites to book
+                                                    <input class="checkbox" type="checkbox" id="selectAll"  v-model="selectAll" @change="updatePrices2()"> 
+                                                </th>
                                             </tr>
                                         </thead>
                                             <tbody><template v-for="campsite in booking.campsites">
@@ -282,20 +284,19 @@
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="form-group">
-                                        <div class="checkbox">
-                                            <label><input type="checkbox" v-model="checked"/>Override price charged</label>
-                                        </div>
-                                        <div class="input-group" v-if="checked">
-                                        <span class="input-group-addon">AUD <i class="fa fa-usd"></i></span>
-                                        <input type="text" class="form-control" :placeholder="0|formatMoney(2)" v-model="booking.override_price">
+                                        <input type="checkbox" name="overrideCharge" class="form control" v-model="overrideCharge"/>
+                                        <label for="OverrideCharge">Override price charged </label>
+                                        <div class="input-group" v-if="overrideCharge">
+                                            <span class="input-group-addon">AUD <i class="fa fa-usd"></i></span>
+                                            <input type="text" class="form-control" name="overridePrice" :placeholder="0|formatMoney(2)" v-model="booking.override_price">
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <div class="form-group" v-if="checked">
-                                        <reason type="discount" v-model="booking.override_reason" ref="reason" ></reason>
+                                    <div class="form-group" v-if="overrideCharge">
+                                        <reason type="discount" name="overrideReason" v-model="booking.override_reason" ref="reason" required="true"></reason>
                                     </div>
                                 </div>
                             </div>
@@ -347,7 +348,7 @@ export default {
     data:function () {
         let vm =this;
         return {
-            checked: false,
+            overrideCharge: false,
             isModalOpen:false,
             bookingForm:null,
             countries:[],
@@ -596,6 +597,16 @@ export default {
                     });
                 }
             }
+        },
+        updatePrices2:function (){
+            let vm=this;
+            var campsite_ids = [];
+            vm.booking.campsites.forEach(function (value) {
+                vm.selected_campsites.push(value.id);
+                value.is_selected = true;
+            });
+            console.log(vm.selected_campsites);
+            vm.updatePrices();           
         },
         fetchCountries:function (){
             let vm =this;
@@ -975,7 +986,7 @@ export default {
         },
         isFormValid:function () {
             let vm =this;
-            return (vm.validateParkEntry() && $(vm.bookingForm).valid());
+            return (vm.validateParkEntry() && vm.overrideChargeReason() && $(vm.bookingForm).valid());
         },
         validateParkEntry:function () {
             let vm = this;
@@ -984,6 +995,15 @@ export default {
             }, 0);
 
             return (validRegos == vm.parkEntryVehicles.length);
+        },
+        overrideChargeReason:function () {
+            let vm = this;
+            if(vm.overrideCharge){
+                if(vm.booking.override_price != null && vm.booking.override_reason == "") {                   
+                    return false;
+                } 
+            }
+            return true;
         },
         addFormValidations: function() {
             $(this.bookingForm).validate({
