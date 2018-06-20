5a2a7bb291156cddde8f3fa3b4b128abbe55dbd7
Indrayani Jadhav <indrayanijadhav@aws-mate-003.aws>
2018-06-20 08:55:24 +0800

    wildlifecompliance:4922 Internal user to propose decline

--- wildlifecompliance/components/applications/admin.py
+++ wildlifecompliance/components/applications/admin.py
@@ -17,6 +17,10 @@ class ApplicationDocumentInline(admin.TabularInline):
 class ApplicationTypeAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(models.ApplicationDeclinedDetails)
+class ApplicationDeclinedDetails(admin.ModelAdmin):
+    pass
+
 @admin.register(models.ApplicationGroupType)
 class ApplicationGroupTypeAdmin(admin.ModelAdmin):
     filter_horizontal = ('members',)
