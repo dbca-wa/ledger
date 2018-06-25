3b9d28aa3ee11887ab37c8f21c3e8312102f2838
Jawaid Mushtaq <jawaidm@dbca.wa.gov.au>
2018-06-25 16:43:51 +0800

    replaced checkbox disabled with oncommit to allow POST

--- disturbance/frontend/disturbance/src/components/forms/checkbox.vue
+++ disturbance/frontend/disturbance/src/components/forms/checkbox.vue
@@ -3,18 +3,8 @@
         <div class="form-group">
             <div class="checkbox">
                 <label :id="id">
-                    <input @onclick="isClickable" ref="Checkbox" :name="name" type="checkbox" data-parsley-required :data-conditions="options" @change="handleChange" :checked="isChecked" :required="isRequired"/>
-
-                    <!-- necessary because forms do not send disabled checkbox's -->
-                    <!--
-                    <div v-if="readonly">
-                        <input :disabled="readonly" ref="Checkbox" :name="name" type="checkbox" data-parsley-required :data-conditions="options" @change="handleChange" :checked="isChecked" :required="isRequired"/>
-                    </div>
-                    <div v-else>
-                        <input onclick="this.checked=!this.checked;" ref="Checkbox" :name="name" type="checkbox" data-parsley-required :data-conditions="options" @change="handleChange" :checked="isChecked" :required="isRequired"/>
-                    </div>
-                    -->
-                    {{ label }}
+                <input :onclick="isClickable" ref="Checkbox" :name="name" type="checkbox" data-parsley-required :data-conditions="options" @change="handleChange" :checked="isChecked" :required="isRequired"/>
+                {{ label }}
                 <template v-if="help_text">
                   <HelpText :help_text="help_text" />
                 </template>
@@ -28,20 +18,18 @@ import HelpText from './help_text.vue'
 export default {
   props: ['name', 'label', 'value', 'id', 'help_text', 'conditions', "handleChange","readonly", "isRequired"],
   components: {HelpText},
+  data: function() {
+    let vm = this;
+    return {
+      isClickable: "return true;",
+	}
+  },
   computed: {
     isChecked: function() {
       return (this.value == 'on');
     },
     options: function() {
       return JSON.stringify(this.conditions);
-    },
-    isClickable: function() {
-      return (this.checked=!this.checked);
-    }
-  },
-  methods: {
-    isClickable: function() {
-      return (this.checked=!this.checked);
     }
   },
   mounted:function () {
@@ -51,18 +39,13 @@ export default {
           var e = document.createEvent('HTMLEvents');
           e.initEvent('change', true, true);
 
-          var disabledStatus = input.disabled;
-          try {
-              /* Firefox will not fire events for disabled widgets, so (temporarily) enabling them */
-              if(disabledStatus) {
-                  input.disabled = false;
-              }
-              input.dispatchEvent(e);
-          } finally {
-              if(disabledStatus) {
-                  input.disabled = true;
-              }
-          }
+          /* replacing input.disabled with onclick because disabled checkbox does NOT get posted with form on submit */
+          if(vm.readonly) {
+              vm.isClickable = "return false;";
+          } else {
+              vm.isClickable = "return true;";
+		  }
+          input.dispatchEvent(e);
       }
   }
 }
