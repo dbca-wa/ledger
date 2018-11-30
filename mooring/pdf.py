82513f8b07af314971f7d83ff2da69dc33f4e88c
jason.moore@dbca.wa.gov.au <Jason Moore>
2018-11-30 14:40:27 +0800

    Remove Contact Information From Footer

--- mooring/pdf.py
+++ mooring/pdf.py
@@ -98,14 +98,14 @@ def _create_letter_header_footer(canvas, doc):
     canvas.setFont(DEFAULT_FONTNAME, SMALL_FONTSIZE)
     canvas.setFillColor(HexColor(LETTER_BLUE_FONT))
 
-    canvas.drawRightString(current_x, current_y, DPAW_URL)
-    canvas.drawRightString(current_x, current_y + SMALL_FONTSIZE,
-                           'Phone: {} Fax: {} Email: {}'.format(DPAW_PHONE, DPAW_FAX, DPAW_EMAIL))
-    canvas.drawRightString(current_x, current_y + SMALL_FONTSIZE * 2, DPAW_PO_BOX)
+    #canvas.drawRightString(current_x, current_y, DPAW_URL)
+    #canvas.drawRightString(current_x, current_y + SMALL_FONTSIZE,
+    #                       'Phone: {} Fax: {} Email: {}'.format(DPAW_PHONE, DPAW_FAX, DPAW_EMAIL))
+    #canvas.drawRightString(current_x, current_y + SMALL_FONTSIZE * 2, DPAW_PO_BOX)
 
-    canvas.setFont(BOLD_ITALIC_FONTNAME, SMALL_FONTSIZE)
+    #canvas.setFont(BOLD_ITALIC_FONTNAME, SMALL_FONTSIZE)
 
-    canvas.drawRightString(current_x, current_y + SMALL_FONTSIZE * 3, DPAW_BUSINESS)
+    #canvas.drawRightString(current_x, current_y + SMALL_FONTSIZE * 3, DPAW_BUSINESS)
 
 
 def create_confirmation(confirmation_buffer, booking, mooring_bookings):
