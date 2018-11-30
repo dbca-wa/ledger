467ea7c9dfc6cc1ecda444803539df6e76a4e239
shaynesharpe <shayne.sharpe@dbca.wa.gov.au>
2018-11-30 16:36:42 +0800

    #4925.Fixed notifications for group email.

--- wildlifecompliance/components/organisations/emails.py
+++ wildlifecompliance/components/organisations/emails.py
@@ -72,7 +72,7 @@ class OrganisationIdUploadNotificationEmail(TemplateEmailBase):
     txt_template = 'wildlifecompliance/emails/organisation_id_upload_notification.txt'
 
 
-def send_organisation_id_upload_email_notification(emails, organisation, request):
+def send_organisation_id_upload_email_notification(emails, organisation, org_contact, request):
     email = OrganisationIdUploadNotificationEmail()
 
     context = {
@@ -81,7 +81,7 @@ def send_organisation_id_upload_email_notification(emails, organisation, request
 
     msg = email.send(emails, context=context)
     sender = request.user if request else settings.DEFAULT_FROM_EMAIL
-    _log_org_email(msg, organisation, sender=sender)
+    _log_org_email(msg, organisation, org_contact, sender=sender)
 
 
 def send_organisation_reinstate_email_notification(linked_user,linked_by,organisation,request):
