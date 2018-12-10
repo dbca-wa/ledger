efa5c9c1f63cf60f6d27808f196a484e0ac427ea
jason.moore@dbca.wa.gov.au <Jason Moore>
2018-12-10 16:32:27 +0800

    fix distance

--- mooring/api.py
+++ mooring/api.py
@@ -1547,20 +1547,23 @@ class BaseAvailabilityViewSet(viewsets.ReadOnlyModelViewSet):
 class BaseAvailabilityViewSet2(viewsets.ReadOnlyModelViewSet):
     queryset = MooringArea.objects.all()
     serializer_class = MooringAreaSerializer
- 
+
+
     def distance(self,origin, destination):
          lat1, lon1 = origin
          lat2, lon2 = destination
          radius = 6371 # km
 
-         dlat = math.radians(lat2 - lat1)
-         dlon = math.radians(lon2 - lon1)
-         a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
-             math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
-             math.sin(dlon / 2) * math.sin(dlon / 2))
-         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-         d = radius * c
-         return d
+
+         dlon = lon2 - lon1
+         dlat = lat2 - lat1
+         a = (math.sin(dlat/2))**2 + math.cos(lat1) * math.cos(lat2) * (math.sin(dlon/2))**2
+         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+         distance = radius * c
+
+         return distance
+
+
  
     def retrieve(self, request, pk=None, ratis_id=None, format=None, show_all=False):
         """Fetch full campsite availability for a campground."""
