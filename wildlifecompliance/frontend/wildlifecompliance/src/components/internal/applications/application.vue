d2ecbb2fa03316c248aa643154d2423b774296fb
Wilson Cheung <wilson.cheung86@gmail.com>
2018-12-13 14:33:20 +0800

    WildlifeCompliance: revert add entry comms log change from previous commit

--- wildlifecompliance/frontend/wildlifecompliance/src/components/internal/applications/application.vue
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/internal/applications/application.vue
@@ -3,7 +3,7 @@
             <div class="row">
         <h3>Application: {{ application.lodgement_number }}</h3>
         <div class="col-md-3">
-            <CommsLogs :comms_url="comms_url" :logs_url="logs_url" :comms_add_url="comms_add_url" :disable_add_entry="true"/>
+            <CommsLogs :comms_url="comms_url" :logs_url="logs_url" :comms_add_url="comms_add_url" :disable_add_entry="false"/>
             <div class="row" v-if="canSeeSubmission">
                 <div class="panel panel-default">
                     <div class="panel-heading">
