fa5e1d834324bbedbb1f8ab4789909dd8ecc264e
root <root@kens-mate-001.corporateict.domain>
2018-12-18 14:53:25 +0800

    added management commands and logging

--- wildlifecompliance/components/applications/views.py
+++ wildlifecompliance/components/applications/views.py
@@ -73,68 +73,68 @@ class ApplicationSuccessView(TemplateView):
         delete_session_application(request.session)
         return render(request, self.template_name, context)
 
-def update_workbooks(request):
-    writer = ExcelWriter()
-    writer.update_workbooks()
-
-def export_applications(request):
-    filename = 'wildlife_compliance_applications_{}.xls'.format(datetime.now().strftime('%Y%m%dT%H%M%S'))
-    response = HttpResponse(content_type='application/ms-excel')
-    response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
-
-    wb = xlwt.Workbook(encoding='utf-8')
-    ws = wb.add_sheet('Applications')
-
-    # Sheet header, first row
-    row_num = 0
-
-    font_style = xlwt.XFStyle()
-    font_style.font.bold = True
-
-    applications = Application.objects.filter(id__in=[121, 122, 123])
-
-    for application in applications:
-        s=serialize_export(application)
-
-
-        columns = unique_column_names()
-        names = [row['name'] for row in s]
-        row_num += 1
-        for col_num in range(len(columns)):
-            ws.write(row_num, col_num, columns[col_num], font_style)
-
-
-        keys = [row['key'] for row in s]
-
-#activity = [row['activity'] for row in s]
-#purpose = [row['purpose'] for row in s]
-        labels = [row['label'] for row in s]
-        for col_num in range(len(keys)):
-            ws.write(row_num, col_num, keys[col_num], font_style)
-
-        row_num += 1
-        for col_num in range(len(keys)):
-            ws.write(row_num, col_num, activity[col_num], font_style)
-
-        row_num += 1
-        for col_num in range(len(keys)):
-            ws.write(row_num, col_num, purpose[col_num], font_style)
-
-        row_num += 1
-        for col_num in range(len(keys)):
-            ws.write(row_num, col_num, labels[col_num], font_style)
-        row_num += 1
-
-# Sheet body, remaining rows
-        font_style = xlwt.XFStyle()
-
-        rows = [row['key'] for row in s]
-        for row in a:
-            row_num += 1
-            col_items = [item['value'] for item in s]
-            for col_num in range(len(col_items)):
-                ws.write(row_num, col_num, col_items[col_num], font_style)
-
-    wb.save(response)
-    return response
+#def update_workbooks(request):
+#    writer = ExcelWriter()
+#    writer.update_workbooks()
+#
+#def export_applications(request):
+#    filename = 'wildlife_compliance_applications_{}.xls'.format(datetime.now().strftime('%Y%m%dT%H%M%S'))
+#    response = HttpResponse(content_type='application/ms-excel')
+#    response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+#
+#    wb = xlwt.Workbook(encoding='utf-8')
+#    ws = wb.add_sheet('Applications')
+#
+#    # Sheet header, first row
+#    row_num = 0
+#
+#    font_style = xlwt.XFStyle()
+#    font_style.font.bold = True
+#
+#    applications = Application.objects.filter(id__in=[121, 122, 123])
+#
+#    for application in applications:
+#        s=serialize_export(application)
+#
+#
+#        columns = unique_column_names()
+#        names = [row['name'] for row in s]
+#        row_num += 1
+#        for col_num in range(len(columns)):
+#            ws.write(row_num, col_num, columns[col_num], font_style)
+#
+#
+#        keys = [row['key'] for row in s]
+#
+##activity = [row['activity'] for row in s]
+##purpose = [row['purpose'] for row in s]
+#        labels = [row['label'] for row in s]
+#        for col_num in range(len(keys)):
+#            ws.write(row_num, col_num, keys[col_num], font_style)
+#
+#        row_num += 1
+#        for col_num in range(len(keys)):
+#            ws.write(row_num, col_num, activity[col_num], font_style)
+#
+#        row_num += 1
+#        for col_num in range(len(keys)):
+#            ws.write(row_num, col_num, purpose[col_num], font_style)
+#
+#        row_num += 1
+#        for col_num in range(len(keys)):
+#            ws.write(row_num, col_num, labels[col_num], font_style)
+#        row_num += 1
+#
+## Sheet body, remaining rows
+#        font_style = xlwt.XFStyle()
+#
+#        rows = [row['key'] for row in s]
+#        for row in a:
+#            row_num += 1
+#            col_items = [item['value'] for item in s]
+#            for col_num in range(len(col_items)):
+#                ws.write(row_num, col_num, col_items[col_num], font_style)
+#
+#    wb.save(response)
+#    return response
 
