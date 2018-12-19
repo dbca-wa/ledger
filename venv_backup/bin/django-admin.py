546184b9916094a35d5d28556784a2079faa45e6
Indrayani Jadhav <indra.jadhav@gmail.com>
2018-12-19 09:59:14 +0800

    Wildlifecompliance-Removing backup folder

--- venv_backup/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/corporateict.domain/indrayanijadhav/projects/ledger/venv/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
