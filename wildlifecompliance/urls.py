7ad0f3e4458ae968444bbb49d108db864131feec
Wilson Cheung <wilson.cheung86@gmail.com>
2018-12-27 17:03:35 +0800

    WildlifeCompliance: update contact us and further info links

--- wildlifecompliance/urls.py
+++ wildlifecompliance/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls import url, include
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from django.conf.urls.static import static
 from rest_framework import routers
 
