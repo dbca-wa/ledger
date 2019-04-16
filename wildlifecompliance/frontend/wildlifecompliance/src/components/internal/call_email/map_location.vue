fbd515ab889d62c9a166e6af6b5bb01cdfd29b7e
Katsu <katsufumi.shibata@dbca.wa.gov.au>
2019-04-16 10:30:40 +0800

    XDisplay location map. But layout broken...

---
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/internal/call_email/map_location.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+    <div class="container">
+        <div id="map" class="map"></div>
+    </div>
+</template>
+
+<script>
+var L = require('leaflet');
+module.exports = {
+    data: function(){
+        return {
+            map: null,
+            tileLayer: null,
+            layers: [],
+        }
+    },
+    mounted(){
+        this.initMap();
+        this.initLayers();
+    },
+    methods: {
+        initMap() {
+            this.map = L.map('map').setView([38.63, -90.23], 12);
+            this.tileLayer = L.tileLayer(
+                'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+                {
+                    maxZoom: 18,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+                } 
+            );
+            this.tileLayer.addTo(this.map);
+        },
+        initLayers(){
+
+        },
+    },
+}
+</script>
+
+<style lang="css">
+.map { 
+    height: 400px; 
+    width: 400px; 
+}
+</style>
