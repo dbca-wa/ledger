9136b3609ea99da3b50d9a26b6fdf5d06e6eb472
Katsu <katsufumi.shibata@dbca.wa.gov.au>
2019-05-07 14:21:57 +0800

    Replace openlayers by leaflet

--- wildlifecompliance/frontend/wildlifecompliance/src/components/internal/call_email/map_locations.vue
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/internal/call_email/map_locations.vue
@@ -1,285 +1,179 @@
-
 <template lang="html">
-    <div>
-        <div id="mapOL">
-        </div>
+    <div class="container">
+        <div id="map"></div>
+
     </div>
 </template>
 
 <script>
-import pin from '../../../assets/pin.svg';
-import Map from 'ol/Map.js';
-import tilegridWMTS from 'ol/tilegrid/WMTS.js';
-import { getWidth, getTopLeft } from 'ol/extent.js';
-import View from 'ol/View.js';
-import { defaults as defaultControls, ScaleLine} from 'ol/control.js';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
-import { XYZ, OSM, WMTS } from 'ol/source';
-import { get, addProjection, addCoordinateTransforms, transform, toLonLat, fromLonLat } from 'ol/proj.js';
-import Feature from 'ol/Feature.js';
-import Overlay from 'ol/Overlay';
-import Point from 'ol/geom/Point.js';
-import VectorSource from 'ol/source/Vector.js';
-import { Icon, Style } from 'ol/style.js';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-import 'ol/ol.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'ol-geocoder/dist/ol-geocoder.css';
-import 'awesomplete/awesomplete.css';
-import { mapState, mapGetters, mapActions, mapMutations } from "vuex";
-
-export default {
-    name: "map-openlayers-dashboard",
-    data: function(){
-        const defaultCentre = [13775786.985667605, -2871569.067879858];
-
-        return {
-            defaultCenter: defaultCentre,
-            projection: null,
-            map: null,
-            popup: null,
-            element: null,
-            marker_locked: false,
-            base_layer: 'osm',
-            awe: null,
-            suggest_list: [],
-            feature_marker: null,
-        };
-    },
-    mounted: function(){
-        console.debug('Start loading map');
-        this.initMap();
-        this.setBaseLayer('osm');
-        this.addMarker();
-        console.debug('End loading map');
+L.TileLayer.WMTS = L.TileLayer.extend({
+    defaultWmtsParams: {
+        service: 'WMTS',
+        request: 'GetTile',
+        version: '1.0.0',
+        layers: '',
+        styles: '',
+        tilematrixSet: '',
+        format: 'image/jpeg'
     },
 
-    methods: {
-        moveMapCentre: function(coordinates){
-            var self = this;
-            var view = self.map.getView();
-            view.animate({
-                center: fromLonLat(coordinates),
-                zoom: 14,
-                duration: 1000
-            });
-        },
-        setBaseLayer: function(selected_layer_name){
-            this.map.getLayers().forEach(function(layer){
-                if (layer.type == 'TILE')
-                {
-                    if (layer.get('name') == selected_layer_name){
-                        layer.setVisible(true);
-                    }
-                    else {
-                        layer.setVisible(false);
-                    }
-                }
-            });
-            if (selected_layer_name == 'sat') {
-                $('#basemap_sat').hide();
-                $('#basemap_osm').show();
+    initialize: function (url, options) { // (String, Object)
+        this._url = url;
+        var wmtsParams = L.extend({}, this.defaultWmtsParams);
+        var tileSize = options.tileSize || this.options.tileSize;
+        if (options.detectRetina && L.Browser.retina) {
+            wmtsParams.width = wmtsParams.height = tileSize * 2;
+        } else {
+            wmtsParams.width = wmtsParams.height = tileSize;
+        }
+        for (var i in options) {
+            // all keys that are not TileLayer options go to WMTS params
+            if (!this.options.hasOwnProperty(i) && i!="matrixIds") {
+                wmtsParams[i] = options[i];
             }
-            else {
-                $('#basemap_osm').hide();
-                $('#basemap_sat').show();
-            }
-        },
-        addMarker: function(){
-            var iconFeature = new Feature({
-                geometry: new Point(transform([
-                    null, null
-                    ], 'EPSG:4326', 'EPSG:3857')),
-                });
-            this.feature_marker = iconFeature;
+        }
+        this.wmtsParams = wmtsParams;
+        this.matrixIds = options.matrixIds||this.getDefaultMatrix();
+        L.setOptions(this, options);
+    },
 
-            var iconStyle = new Style({
-                image: new Icon({
-                    anchor: [16, 32],
-                    anchorXUnits: 'pixels',
-                    anchorYUnits: 'pixels',
-                    opacity: 1,
-                    src: pin,
-                    scale: 1
-                })
-            });
+    onAdd: function (map) {
+        this._crs = this.options.crs || map.options.crs;
+        L.TileLayer.prototype.onAdd.call(this, map);
+    },
 
-            iconFeature.setStyle(iconStyle);
+    getTileUrl: function (coords) { // (Point, Number) -> String
+        var tileSize = this.options.tileSize;
+        var nwPoint = coords.multiplyBy(tileSize);
+        nwPoint.x+=1;
+        nwPoint.y-=1;
+        var sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
+        var zoom = this._tileZoom;
+        var nw = this._crs.project(this._map.unproject(nwPoint, zoom));
+        var se = this._crs.project(this._map.unproject(sePoint, zoom));
+        var tilewidth = se.x-nw.x;
+        //zoom = this._map.getZoom();
+        var ident = this.matrixIds[zoom].identifier;
+        var tilematrix = this.wmtsParams.tilematrixSet + ":" + ident;
+        var X0 = this.matrixIds[zoom].topLeftCorner.lng;
+        var Y0 = this.matrixIds[zoom].topLeftCorner.lat;
+        var tilecol=Math.floor((nw.x-X0)/tilewidth);
+        var tilerow=-Math.floor((nw.y-Y0)/tilewidth);
+        var url = L.Util.template(this._url, {s: this._getSubdomain(coords)});
+        return url + L.Util.getParamString(this.wmtsParams, url) + "&tilematrix=" + tilematrix + "&tilerow=" + tilerow +"&tilecol=" + tilecol;
+        /*
+        var tileBounds = this._tileCoordsToBounds(coords);
+        var zoom = this._tileZoom;
+        var nw = this._crs.project(tileBounds.getNorthWest());
+        var se = this._crs.project(tileBounds.getSouthEast());
+        var tilewidth = se.x-nw.x;
+        var ident = this.matrixIds[zoom].identifier;
+        var X0 = this.matrixIds[zoom].topLeftCorner.lng;
+        var Y0 = this.matrixIds[zoom].topLeftCorner.lat;
+        var tilecol=Math.floor((nw.x+1-X0)/tilewidth);
+        var tilerow=-Math.floor((nw.y-1-Y0)/tilewidth);
+        var url = L.Util.template(this._url, {s: this._getSubdomain(coords)});
+        console.log(L.Util.getParamString(this.wmtsParams, url) + "&tilematrix=" + ident + "&tilerow=" + tilerow +"&tilecol=" + tilecol );
+        return url + L.Util.getParamString(this.wmtsParams, url) + "&tilematrix=" + ident + "&tilerow=" + tilerow +"&tilecol=" + tilecol ;
+        */
+    },
 
-            var vectorSource = new VectorSource({
-               features: [iconFeature]
-            });
-
-            var vectorLayer = new VectorLayer({
-               source: vectorSource
-            });
-
-            this.map.addLayer(vectorLayer);
-        },
-
-        initMap: function(){
-            this.projection = get('EPSG:3857');
-            this.matrixSet = 'mercator';
-
-            this.projectionExtent = this.projection.getExtent();
-            var size = getWidth(this.projectionExtent) / 256;
-            this.matrixSet = 'mercator';
-            this.resolutions = new Array(21);
-            this.matrixIds = new Array(21);
-            for (var z = 0; z < 21; ++z) {
-                // generate resolutions and matrixIds arrays for this WMTS
-                this.resolutions[z] = size / Math.pow(2, z);
-                this.matrixIds[z] = this.matrixSet + ':' + z;
-            }
-            var tileGrid = new tilegridWMTS({
-                origin: getTopLeft(this.projectionExtent),
-                resolutions: this.resolutions,
-                matrixIds: this.matrixIds
-            });
-
-            var rasterLayer = new TileLayer({source: new OSM()});
-            var rasterLayerSat = new TileLayer({
-                    source: new WMTS({
-                        url: 'https://kmi.dpaw.wa.gov.au/geoserver/gwc/service/wmts',
-                        format: 'image/png',
-                        layer: 'public:mapbox-satellite',
-                        matrixSet: this.matrixSet,
-                        projection: this.projection,
-                        tileGrid: tileGrid
-                    })
-            });
-            rasterLayer.set('name', 'osm');
-            rasterLayerSat.set('name', 'sat');
-            rasterLayer.set('button_id', 'basemap_osm');
-            rasterLayerSat.set('button_id', 'basemap_satellite');
-
-            this.map = new Map({
-                controls: defaultControls().extend([
-                    new ScaleLine({
-                        units: 'metric'
-                    })
-                ]),
-                target: 'mapOL',
-                layers: [
-                    rasterLayer,
-                    rasterLayerSat,
-                ],
-                view: new View({
-                    center: [0, 0],
-                    zoom: 5
-                })
-            });
-            this.map.getView().setCenter(this.defaultCenter);
-            this.map.on('click', this.mapClickHandler);
-
-            this.element = document.getElementById('popup');
-
-            this.popup = new Overlay({
-                element: this.element,
-                positioning: 'bottom-center',
-                stopEvent: false,
-                offset: [0, -25]
-            });
-            this.map.addOverlay(this.popup);
-        },
-        /* this function stores the coordinates into the vuex, then call refresh marker function */
-        relocateMarker: function(coords_3857){ 
-            let coords_4326 = transform([coords_3857[0], coords_3857[1]], 'EPSG:3857', 'EPSG:4326');
-            this.setLocationPoint(coords_4326);
-            this.refreshMarkerLocation();
-            this.reverseGeocoding(coords_4326);
-        },
-        relocateMarker4326: function(coords_4326){
-            this.relocateMarker(transform([coords_4326[0], coords_4326[1]], 'EPSG:4326', 'EPSG:3857'));
-        },
-        mapClickHandler: function(e){
-            var coordinate = e.coordinate;
-            console.debug(coordinate);
-
-            var lnglat_4326 = transform([coordinate[0], coordinate[1]], 'EPSG:3857', 'EPSG:4326')
-
-            var feature = this.map.forEachFeatureAtPixel(e.pixel,
-                function(feature) {
-                    return feature;
-            });
-            if (feature) {
-                /* User clicked on a feature, such as a marker */
-                var coordinates = feature.getGeometry().getCoordinates();
-                this.popup.setPosition(coordinates);
-                $(this.element).popover({
-                    placement: 'top',
-                    html: true,
-                    content: '<p>' 
-                    + this.call_email.location.properties.street + ' ' + this.call_email.location.properties.town_suburb + '<br />'
-                    + this.call_email.location.properties.state + ' ' + this.call_email.location.properties.postcode + ' ' + this.call_email.location.properties.country 
-                    + '</p>',
-                });
-                $(this.element).popover('show');
-            } else {
-                /* User clicked on a map, not on any feature */
-                if(!this.marker_locked){
-                    this.relocateMarker(coordinate);
-                }
-                $(this.element).popover('destroy');
+    setParams: function (params, noRedraw) {
+        L.extend(this.wmtsParams, params);
+        if (!noRedraw) {
+            this.redraw();
+        }
+        return this;
+    },
+    
+    getDefaultMatrix : function () {
+        /**
+         * the matrix3857 represents the projection 
+         * for in the IGN WMTS for the google coordinates.
+         */
+        var matrixIds3857 = new Array(22);
+        for (var i= 0; i<22; i++) {
+            matrixIds3857[i]= {
+                identifier    : "" + i,
+                topLeftCorner : new L.LatLng(20037508.3428,-20037508.3428)
             };
         }
+        return matrixIds3857;
+    }
+});
+
+L.tileLayer.wmts = function (url, options) {
+    return new L.TileLayer.WMTS(url, options);
+};
+
+module.exports = {
+    data: function(){
+        return {
+            map: null,
+            tileLayer: null,
+            tileLayerSat: null,
+            layers: [],
+            popup: null,
+        }
+    },
+    mounted(){
+        this.initMap();
+        this.initLayers();
+    },
+    methods: {
+        onClick(e){
+            this.popup
+            .setLatLng(e.latlng)
+            .setContent(e.latlng.toString())
+            .openOn(this.map);
+        },
+        initMap(){
+            console.log('Start initMap()');
+
+            this.map = L.map('map').setView([-31.9505, 115.8605], 4);
+            this.tileLayer = L.tileLayer(
+                'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                {
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, contributiors',
+                }
+            );
+            this.tileLayer.addTo(this.map);
+
+            this.tileLayerSat = L.tileLayer.wmts(
+                'https://kmi.dpaw.wa.gov.au/geoserver/gwc/service/wmts',
+                {
+                    layer: 'public:mapbox-satellite',
+                    //jstyle: 'normal',
+                    //tilematrixSet: 'EPSG:3857',
+                    tilematrixSet: 'mercator',
+                    format: 'image/png',
+                }
+            );
+
+
+            //this.tileLayer.addTo(this.map);
+            //this.tileLayerSat.addTo(this.map);
+
+            var basemaps = {"OSM": this.tileLayer, "SAT": this.tileLayerSat};
+            L.control.layers(basemaps).addTo(this.map);
+
+
+            this.popup = L.popup();
+            this.map.on('click', this.onClick);
+        },
+        initLayers(){
+
+        },
     },
 }
 </script>
 
 <style lang="css">
-#mapOL {
-    position: relative;
+#map {
     height: 500px;
-}
-.popover {
-    position: relative;
-}
-#search-box {
-    position: absolute;
-    z-index: 1000;
-    top: 10px;
-    left: 50px;
-}
-#search-input {
-    width: 300px;
-    padding: 5px;
-    -moz-border-radius: 5px;
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-}
-#basemap-button {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    z-index: 1000;
-    -moz-box-shadow: 5px 5px 5px #555;
-    -webkit-box-shadow: 5px 5px 5px #555;
-    box-shadow: 5px 5px 5px #555;
-    -moz-filter: brightness(1.0);
-    -webkit-filter: brightness(1.0);
-    filter: brightness(1.0);
-}
-#basemap_sat,#basemap_osm {
-    border-radius: 5px;
-}
-#basemap-button:hover {
-    cursor: pointer;
-    -moz-filter: brightness(0.9);
-    -webkit-filter: brightness(0.9);
-    filter: brightness(0.9);
-}
-#basemap-button:active {
-    top: 11px;
-    right: 9px;
-    -moz-box-shadow: 2px 2px 2px #555;
-    -webkit-box-shadow: 2px 2px 2px #555;
-    box-shadow: 2px 2px 2px #555;
-    -moz-filter: brightness(0.8);
-    -webkit-filter: brightness(0.8);
-    filter: brightness(0.8);
-}
-#location_address_field {
-    resize: vertical;
+    margin-bottom: 50px;
 }
 </style>
