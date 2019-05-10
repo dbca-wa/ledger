4c4f7c25502bbb398e59bb6de5fcba1eb4c349d4
Katsu <katsufumi.shibata@dbca.wa.gov.au>
2019-05-10 10:45:47 +0800

    Clean up files

--- wildlifecompliance/frontend/wildlifecompliance/src/components/internal/call_email/call_email.vue
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/internal/call_email/call_email.vue
@@ -131,8 +131,7 @@
 import Vue from "vue";
 import FormSection from "@/components/compliance_forms/section.vue";
 import CommsLogs from "@common-components/comms_logs.vue";
-//import MapLocation from "./map_location.vue";
-import MapLocation from "./map_location_leaflet.vue";
+import MapLocation from "./map_location.vue";
 import { api_endpoints, helpers } from "@/utils/hooks";
 import utils from "@/components/external/utils";
 import { mapState, mapGetters, mapActions, mapMutations } from "vuex";
