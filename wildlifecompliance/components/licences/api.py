a07cc7c6baf39ec1f73dfb66cce6d282f3c2370e
Wilson Cheung <wilson.cheung86@gmail.com>
2019-06-18 16:05:19 +0800

    WildlifeCompliance: external licence dashboard filter out Licences without an ACCEPTED ApplicationSelectedActivity

--- wildlifecompliance/components/licences/api.py
+++ wildlifecompliance/components/licences/api.py
@@ -180,13 +180,17 @@ class LicencePaginatedViewSet(viewsets.ModelViewSet):
     @list_route(methods=['GET', ])
     def external_datatable_list(self, request, *args, **kwargs):
         self.serializer_class = DTExternalWildlifeLicenceSerializer
+        # Filter for WildlifeLicence objects that have a current application linked with an
+        # ApplicationSelectedActivity that has been ACCEPTED
+        asa_accepted = ApplicationSelectedActivity.objects.filter(
+            processing_status=ApplicationSelectedActivity.PROCESSING_STATUS_ACCEPTED)
         user_orgs = [
             org.id for org in request.user.wildlifecompliance_organisations.all()]
         queryset = WildlifeLicence.objects.filter(
             Q(current_application__org_applicant_id__in=user_orgs) |
             Q(current_application__proxy_applicant=request.user) |
             Q(current_application__submitter=request.user)
-        )
+        ).filter(current_application__in=asa_accepted.values_list('application_id', flat=True))
         # Filter by org
         org_id = request.GET.get('org_id', None)
         if org_id:
