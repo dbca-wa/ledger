abdf29c34d884917add8e02598308cce5eb13360
Katsu <katsufumi.shibata@dbca.wa.gov.au>
2019-07-22 10:25:36 +0800

    Add a validation so that any offender must be either a person or an organisation

--- wildlifecompliance/components/offence/models.py
+++ wildlifecompliance/components/offence/models.py
@@ -122,5 +122,11 @@ class Offender(models.Model):
         else:
             return '---'
 
+    def clean(self):
+        if (self.person and self.organisation) or (not self.person and not self.organisation):
+            raise ValidationError('An offender must be either a person or an organisation.')
+
+        super(Offender, self).clean()
+
 
 
