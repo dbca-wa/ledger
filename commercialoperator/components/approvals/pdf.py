0acc53ffce33578c21523e9c5c10fcf81f10f0db
preranaandure <prerana.andure@dbca.wa.gov.au>
2019-07-26 15:44:11 +0800

    Licence pdf to include position title.

--- commercialoperator/components/approvals/pdf.py
+++ commercialoperator/components/approvals/pdf.py
@@ -431,6 +431,8 @@ def _create_approval_cols(approval_buffer, approval, proposal, copied_to_permit,
     elements.append(Spacer(1, SECTION_BUFFER_HEIGHT))
 
     elements.append(Paragraph('{} {}'.format(user.first_name, user.last_name), styles['Left']))
+    if user.position_title:
+        elements.append(Paragraph('{}'.format(user.position_title), styles['Left']))
     elements.append(Paragraph('As Delegate of CEO', styles['Left']))
     elements.append(Paragraph('Under Section 133(2) of the CALM Act 1984', styles['Left']))
     elements.append(Spacer(1, SECTION_BUFFER_HEIGHT))
