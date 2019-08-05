b65f1f74f5f7be1ab57fee6941d7037123922f74
jason.moore@dbca.wa.gov.au <Jason Moore>
2019-08-05 18:11:44 +0800

    Fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='ledger',
-      version='1.01',
+      version='1.2',
       description='Ledger Payments App',
       url='https://github.com/dbca-wa/ledger',
       author='Department of Parks and Wildlife',
       author_email='asi@dbca.wa.gov.au',
       license='BSD',
-      packages=['ledger','ledger.accounts','ledger.address','ledger.basket','ledget.catalogue','ledger.checkout','ledger.dashboard'],
+      packages=['ledger','ledger.accounts','ledger.address','ledger.basket','ledger.catalogue','ledger.checkout','ledger.dashboard'],
       install_requires=[],
       zip_safe=False)
