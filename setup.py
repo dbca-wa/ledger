f3d5e8dad7b0df8f60292a3e6d4ad3db9f9f014a
jason.moore@dbca.wa.gov.au <Jason Moore>
2019-08-06 09:52:39 +0800

    add ledger folders

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='ledger',
-      version='1.2',
+      version='1.3',
       description='Ledger Payments App',
       url='https://github.com/dbca-wa/ledger',
       author='Department of Parks and Wildlife',
       author_email='asi@dbca.wa.gov.au',
       license='BSD',
-      packages=['ledger','ledger.accounts','ledger.address','ledger.basket','ledger.catalogue','ledger.checkout','ledger.dashboard'],
+      packages=['ledger','ledger.accounts','ledger.address','ledger.basket','ledger.catalogue','ledger.checkout','ledger.dashboard','ledger.emails','ledger.licence','ledger.order','ledger.partner','ledger.payments','ledger.static','ledger.taxonomy','ledger.templates'],
       install_requires=[],
       zip_safe=False)
