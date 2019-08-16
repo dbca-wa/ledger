bc4350666c5ac159e4bb1d5473d1becf20b2bfd3
dpawasi <asi@dpaw.wa.gov.au>
2019-08-16 16:52:16 +0800

    minore

--- commercialoperator/frontend/commercialoperator/src/components/common/tclass/treeview.vue
+++ commercialoperator/frontend/commercialoperator/src/components/common/tclass/treeview.vue
@@ -1,17 +1,20 @@
 <template lang="html">
   <div>
+      <a class="test-url col-sm-4" @click="btn_test($event)">Test URL</a>
+      <input type="button" @click="btn_test($event)" class="btn btn-primary" value="button test"/>
       <div>
-        <label class="control-label">Select Parks (New)</label>
+        <label class="control-label">select parks (new)</label>
         <treeselect
           v-model="value"
           :max-height="200"
           :multiple="true"
           :options="options"
-          :open-on-click="true"
-          :value-consists-of="valueConsistsOf"
+          :open-on-click="false"
+          :value-consists-of="valueconsistsof"
           :always-open="true"
           :clearable="true"
           :flat="false"
+          :appendToBody="false"
           :default-expand-level="0"
         >
 
@@ -20,26 +23,42 @@
           <a href="#" v-if="item.to">{{item.label}} {{item.to}}</a>
         </template>
 
-        <label slot="option-label" slot-scope="{ node, shouldShowCount, count, labelClassName, countClassName }" :class="labelClassName">
-            {{ node.isBranch ? 'Branch' : 'Leaf' }}: {{ node.label }}
-            <span v-if="shouldShowCount" :class="countClassName">({{ count }})</span>
+        <label slot="option-label" slot-scope="{ node, shouldshowcount, count, labelclassname, countclassname }" :class="labelclassname">
+            {{ node.isbranch ? 'branch' : 'leaf' }}: {{ node.label }}
+            <span v-if="shouldshowcount" :class="countclassname">({{ count }})</span>
         </label>
 
-        <div slot="value-label" slot-scope="{ node }">{{ node.raw.customLabel }}</div>
--->
-        <div slot="option-label" slot-scope="{ node }">
-            <div class="form-group">
-                <label class="col-sm-8 control-label">{{ node.raw.label }}</label>
-                <a class="col-sm-4 container-url" href="#https://www.google.com" target="_blank" v-if="node.raw.to">{{node.raw.to}}</a>
-            </div>
-        </div>
+        <div slot="value-label" slot-scope="{ node }">{{ node.raw.customlabel }}</div>
 
-
-<!--
         <template slot="value-label" slot-scope="{ node }">
           <a href="#" v-if="node.raw.to">{{node.raw.label}} {{node.raw.to}}</a>
         </template>
 -->
+        <template slot="option-label" slot-scope="{ node }">
+                <label class="col-sm-10 control-label">{{ node.raw.label }}</label>
+                <!--
+                <a class="container-url col-sm-2" href="#https://www.google.com" target="_blank" v-if="node.raw.to">{{node.raw.to}}</a>
+                <a class="container-url col-sm-2" @click="btn_test()">edit</a>
+                <div class="container-url col-sm-2"><input type="button" @click.stop="btn_test()" class="btn btn-primary" value="button test"/></div>
+                -->
+                <span v-if="node.raw.to">{{node.raw.to}}>
+                    <a id="id_edit_0" class="col-sm-2" v-on:click="btn_test($event)">edit0</a>
+                    <a id="id_edit_1" class="col-sm-2" @click.stop="btn_test($event)">edit1</a>
+                    <a id="id_edit_2" class="col-sm-2" @click.prevent="btn_test($event)">Edit2</a>
+                    <a id="id_edit_3" class="col-sm-2" @click.stop.prevent="btn_test($event)">Edit3</a>
+                    <a id="id_edit_4" class="col-sm-2" @click.prevent.self="btn_test($event)">Edit4</a>
+                    <a id="id_edit_5" class="col-sm-2" @click.self="btn_test($event)">Edit5</a>
+                    <a id="id_edit_6" class="col-sm-2" @click.self.native="btn_test($event)">Edit6</a>
+                    <a id="id_edit_7" class="col-sm-2" @click.self.stop="btn_test($event)">Edit7</a>
+                    <a id="id_edit_8" class="col-sm-2" @click.self.prevent="btn_test($event)">Edit8</a>
+                    <a id="id_edit_9" class="col-sm-2" @click.capture.stop.prevent="btn_test($event)">Edit9</a>
+                    <a id="id_edit_10" class="col-sm-2" @click.self.stop.prevent="btn_test($event)">Edit10</a>
+                    <i id="id_edit_11" class="fa fa-edit" title="Edit Activities 1" @click="btn_test($event)"></i>
+                    <i id="id_edit_12" class="fa fa-edit" title="Edit Activities 2" @click.native.stop.prevent="btn_test($event)"></i>
+                    <i id="id_edit_13" class="fa fa-edit" title="Edit Activities 3" @click.native="btn_test($event)"></i>
+                </span>
+        </template>
+
 
         </treeselect>
       </div>
@@ -102,6 +121,29 @@ export default {
     },
 
     methods:{
+        btn_test:function(e){
+            //e.preventDefault()
+            //e.preventPropagation()
+            this.$emit('click', e);
+            alert("TEST BTN clicked.");
+        }
+    },
+
+    beforeUpdate: function() {
+        var elem=$('#id_edit_0');
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent('click', true, true);
+        elem.dispatchEvent(event);
+
+        event.stopPropagation();
+
+        /*
+        $("#id_edit_0").click(
+            function (e) { 
+                e.stopPropagation(); 
+            }
+        );
+        */
     },
 
     mounted:function () {
@@ -127,11 +169,25 @@ export default {
     }
 */
 
-      $('.vue-treeselect__label-container > .container-url').click(
+/*
+      $('.vue-treeselect__label-container .container-url input').click(
           function(e) {
             e.stopPropagation();
+            alert("1")
           }
       );
+
+      $(".container-url input").on('click', function (e) {
+          e.stopPropagation(); 
+          alert("2")
+      })
+
+      $(".test-url a").on('click', function (e) {
+          alert("TEST URL clicked."); 
+      })
+      //$("#id_edit_0").on('click', function (e) { e.stopPropagation(); })
+*/
+      $("#id_edit_0").click(function (e) { e.stopPropagation(); });
     }
 }
 </script>
