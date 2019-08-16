52286c87c1645ab880d99b7df2ec973aebc19954
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2019-08-16 14:35:58 +0800

    Remove redundant code

--- wildlifecompliance/components/call_email/api.py
+++ wildlifecompliance/components/call_email/api.py
@@ -661,51 +661,6 @@ class CallEmailViewSet(viewsets.ModelViewSet):
             print(traceback.print_exc())
             raise serializers.ValidationError(str(e))
 
-    #def send_mail(self, request, instance, workflow_entry, *args, **kwargs):
-    #    print("send_mail")
-    #    print(request.data)
-    #    try:
-    #        #attachments = []
-    #        #for doc in workflow_entry.documents.all():
-    #         #   attachments.append(doc)
-
-    #        email_group = []
-    #        if request.data.get('assigned_to'):
-    #            try:
-    #                user_id_int = int(request.data.get('assigned_to'))
-    #                email_group.append(EmailUser.objects.get(id=user_id_int))
-    #                # update CallEmail
-    #                instance.assigned_to = (EmailUser.objects.get(id=user_id_int))
-    #            except Exception as e:
-    #                    print(traceback.print_exc())
-    #                    raise
-    #        elif request.data.get('allocated_group'):
-    #            users = request.data.get('allocated_group').split(",")
-    #            for user_id in users:
-    #                if user_id:
-    #                    try:
-    #                        user_id_int = int(user_id)
-    #                        email_group.append(EmailUser.objects.get(id=user_id_int))
-    #                    except Exception as e:
-    #                        print(traceback.print_exc())
-    #                        raise
-    #        else:
-    #            email_group.append(request.user)
-
-    #        # send email
-    #        email_data = send_call_email_forward_email(
-    #        email_group, 
-    #        instance,
-    #        # workflow_entry.documents,
-    #        workflow_entry,
-    #        request)
-
-    #        return email_data
-
-    #    except Exception as e:
-    #        print(traceback.print_exc())
-    #        raise e
-
     @detail_route(methods=['POST', ])
     @renderer_classes((JSONRenderer,))
     def workflow_action(self, request, *args, **kwargs):
@@ -854,17 +809,6 @@ class CasePriorityViewSet(viewsets.ModelViewSet):
         if is_internal(self.request):
             return CasePriority.objects.all()
         return CasePriority.objects.none()
-
-
-# class InspectionTypeViewSet(viewsets.ModelViewSet):
-#     queryset = InspectionType.objects.all()
-#     serializer_class = InspectionTypeSerializer
-
-#     def get_queryset(self):
-#         user = self.request.user
-#         if is_internal(self.request):
-#             return InspectionType.objects.all()
-#         return InspectionType.objects.none()
 
 
 class ReportTypeViewSet(viewsets.ModelViewSet):
