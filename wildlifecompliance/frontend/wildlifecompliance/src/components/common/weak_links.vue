d9ee3f6ecadb0592214a9a1b8ffa2b5522b57aa6
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2019-08-27 18:22:39 +0800

    Format DOM

--- wildlifecompliance/frontend/wildlifecompliance/src/components/common/weak_links.vue
+++ wildlifecompliance/frontend/wildlifecompliance/src/components/common/weak_links.vue
@@ -3,7 +3,6 @@
         <div class="col-sm-4">
             <label for="entity-selector">Select entity</label>
             <select class="form-control" v-model="selectedEntity" id="entity-selector">
-                <!--option disabled value="">Select entity</option-->
                 <option value="call_email">Call / Email</option>
                 <option value="inspection">Inspection</option>
                 <option value="offence">Offence</option>
@@ -11,12 +10,9 @@
             </select>
         </div>
         <div class="col-sm-4">
-            
-            <label for="elemId" class="transparent">Enter search text </label>
-            <!--div class="row"></div-->
             <input :id="elemId" :class="classNames" :readonly="!isEditable" placeholder="Begin typing to search"/>
         </div>
-        <div class="col-sm-3 action-button">
+        <div class="col-sm-3 no-label">
             <a ref="add_weak_link" @click="callCreateWeakLink" class="btn btn-primary btn-block">Add</a>
         </div>
     </div>
@@ -41,13 +37,9 @@ export default {
         }
     },
     props: {
-        // This prop is not used any more.  Instead elemId in the data is used.
-        //elementId: {
-          //  required: false
-        //},
         classNames: {
             required: false,
-            default: "form-control",
+            default: "form-control no-label"
         },
         maxItems: {
             required: false,
@@ -55,8 +47,6 @@ export default {
         },
         search_type: {
             required: false,
-            //default: 'individual' // 'individual' or 'organisation'
-                                  //  This variable can be changed dynamically, for example, by the selection of radio buttons
         },
         isEditable: {
             required: false,
@@ -69,15 +59,7 @@ export default {
         },
     },
     methods: {
-        //clearInput: function(){
-        //    document.getElementById(this.elemId).value = "";
-        //},
-        //setInput: function(weak_link_str){
-        //    document.getElementById(this.elemId).value = weak_link_str;
-        //},
         markMatchedText(original_text, input) {
-            console.log(original_text)
-            console.log(input)
             let ret_text = original_text.replace(new RegExp(input, "gi"), function( a, b) {
                 return "<mark>" + a + "</mark>";
             });
@@ -102,53 +84,6 @@ export default {
                     return ret;
                 },
                 data: (item, input) => {
-                    //if (this.search_type == "individual") {
-                    //    let f_name = item.first_name ? item.first_name : "";
-                    //    let l_name = item.last_name ? item.last_name : "";
-            
-                    //    let full_name = [f_name, l_name].filter(Boolean).join(" ");
-                    //    let email = item.email ? "E:" + item.email : "";
-                    //    let p_number = item.phone_number ? "P:" + item.phone_number : "";
-                    //    let m_number = item.mobile_number ? "M:" + item.mobile_number : "";
-                    //    let dob = item.dob ? "DOB:" + item.dob : "DOB: ---";
-            
-                    //    let full_name_marked = "<strong>" + this.markMatchedText(full_name, input) + "</strong>";
-                    //    let email_marked = this.markMatchedText(email, input);
-                    //    let p_number_marked = this.markMatchedText(p_number, input);
-                    //    let m_number_marked = this.markMatchedText(m_number, input);
-                    //    let dob_marked = this.markMatchedText(dob, input);
-            
-                    //    let myLabel = [
-                    //        full_name_marked,
-                    //        email_marked,
-                    //        p_number_marked,
-                    //        m_number_marked,
-                    //        dob_marked
-                    //    ].filter(Boolean).join("<br />");
-                    //    myLabel = "<div data-item-id=" + item.id + ' data-type="individual">' + myLabel + "</div>";
-            
-                    //    return {
-                    //        label: myLabel, // Displayed in the list below the search box
-                    //        value: [full_name, dob].filter(Boolean).join(", "), // Inserted into the search box once selected
-                    //        id: item.id
-                    //    };
-                    //} else {
-                    //    let name = item.name ? item.name : "";
-                    //    let abn = item.abn ? "ABN:" + item.abn : "";
-            
-                    //    let name_marked = "<strong>" + this.markMatchedText(name, input) + "</strong>";
-                    //    let abn_marked = this.markMatchedText(abn, input);
-            
-                    //    let myLabel = [name_marked, abn_marked].filter(Boolean).join("<br />");
-                    //    myLabel = "<div data-item-id=" + item.id + ' data-type="organisation">' + myLabel + "</div>";
-            
-                    //    return {
-                    //        label: myLabel,
-                    //        value: [name, abn].filter(Boolean).join(", "),
-                    //        id: item.id
-                    //    };
-                    //}
-                    
                     let item_identifier_marked = this.markMatchedText(item.item_identifier, input);
                     let item_description_marked = this.markMatchedText(item.item_description, input);
                     
@@ -156,17 +91,15 @@ export default {
                         item_identifier_marked,
                         item_description_marked,
                     ].filter(Boolean).join("<br />");
-                    //myLabel = "<div data-item-id=" + item.id + ' data-type="weak-link">' + myLabel + "</div>";
                     myLabel = "<div data-item-id=" + item.id + " data-type=" + item.model_name + ">" + myLabel + "</div>";
                     return {
                         label: myLabel,
                         //value: [name, abn].filter(Boolean).join(", "),
-                        value: item.item_identifier,
+                        value: [item.item_identifier, item.item_description].filter(Boolean).join(", "),
                         id: item.id
                     };
                 }
             });
-            console.log(this.awesomplete_obj);
             $(element_search)
                 .on("keyup", (ev) => {
                 var keyCode = ev.keyCode || ev.which;
@@ -209,12 +142,9 @@ export default {
         },
         // Match typed searchTerm against available human-readable object identifiers (get_related_items_identifier) 
         search_available_object_identifiers(searchText) {
-            console.log(searchText)
             let suggested_weak_links = [];
             suggested_weak_links.length = 0;
             this.awesomplete_obj.list = [];
-
-            console.log(this.awesomplete_obj);
 
             /* Cancel all the previous requests */
             if (this.ajax_weak_links != null) {
@@ -237,16 +167,12 @@ export default {
                 data: payload,
                 //contentType: "application/json",
                 success: (data) => {
-                    console.log(data)
                     if (data) {
-                        //let weak_link_results = data.results;
                         let limit = Math.min(this.maxItems, data.length);
                         for (var i = 0; i < limit; i++) {
                         suggested_weak_links.push(data[i]);
                         }
                     }
-                    console.log(suggested_weak_links)
-                    //Object.assign(this.awesomplete_obj.weak_links, suggested_weak_links);
                     this.awesomplete_obj.list = suggested_weak_links;
                     this.awesomplete_obj.evaluate();
                 },
@@ -263,8 +189,8 @@ export default {
 </script>
 
 <style>
-.action-button {
-    margin-top: 5px;
+.no-label {
+    margin-top: 25px;
 }
 .awesomplete > ul {
     margin-top: 2.5em;
