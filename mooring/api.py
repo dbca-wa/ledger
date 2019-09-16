29eca84af27e8524b123cebaeba2158b6c958b77
jason.moore@dbca.wa.gov.au <Jason Moore>
2019-09-16 12:05:20 +0800

    Permssion fix on change booking

--- mooring/api.py
+++ mooring/api.py
@@ -1920,6 +1920,8 @@ class BaseAvailabilityViewSet2(viewsets.ReadOnlyModelViewSet):
                  # compare old and new booking for changes
                  if hashlib.md5(str(current_booking_obj)).hexdigest() == hashlib.md5(str(old_booking_obj)).hexdigest():
                        booking_changed = False
+                 if utils.check_mooring_admin_access(request) is True:
+                       booking_changed = True
                       
         availability = utils.get_campsite_availability(sites_qs, start_date, end_date, ongoing_booking, request)
 
