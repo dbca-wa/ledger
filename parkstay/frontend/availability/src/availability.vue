6d6949e719d1ac7f7181f6620bb7e5df252edd37
mohdbakhrayba <binahmed4@gmail.com>
2019-10-21 13:47:59 +0800

    Restore the position of check online bookable div in template

--- parkstay/frontend/availability/src/availability.vue
+++ parkstay/frontend/availability/src/availability.vue
@@ -88,9 +88,9 @@
                 </div>
             </div>
         </div>
-
+            <div class="row" v-show="status == 'online'">
             <div v-if="long_description" class="columns small-12 medium-12 large-12">
-                <div class="row"><div class="row" v-show="status == 'online'">
+                <div class="row">
                     <div class="columns small-6 medium-6 large-3">
                         <button type="button" class="button formButton" @click="toggleMoreInfo">
                             More Information &nbsp;&nbsp;
