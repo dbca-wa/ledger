e3b9a2cf3df4983bab381a4ccc854dec9e574de2
Ashley Felton <ashley@ropable.com>
2019-12-10 11:39:34 +0800

    Update parkfinder.vue to display correct 'Book now/More info' button.

--- parkstay/frontend/exploreparks/src/parkfinder.vue
+++ parkstay/frontend/exploreparks/src/parkfinder.vue
@@ -627,7 +627,6 @@ export default {
                 return el['properties']['name'] == place;
             });
             if (target) {
-                //console.log('Search suggestion!')
                 var view = this.olmap.getView();
                 // zoom slightly closer in for campgrounds
                 var resolution = vm.resolutions[10];
@@ -651,7 +650,6 @@ export default {
                         // when you pass control of the popup element to OpenLayers :(
                         $("#mapPopupName")[0].innerHTML = feature.get('name');
                         if (feature.get('images')) {
-                            // console.log(feature.get('images')[0].image);
                             $("#mapPopupImage").attr('src', feature.get('images')[0].image);
                             $("#mapPopupImage").show();
                         } else {
@@ -667,15 +665,8 @@ export default {
                         // Disabled below line,as api is being used to differentiate btw offline and online site
                         $("#mapPopupInfo").attr('href', feature.get('info_url'));
 
-
                         // Made changes to show only one button -->
                         $("#mapPopupBookInfo").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
-
-                        // if (feature.get('campground_type') == 0) {
-                        //    $("#mapPopupBook").show();
-                        // } else {
-                        //    $("#mapPopupBook").hide();
-                        // }
 
                     }, 1000);
                 } */
@@ -696,8 +687,6 @@ export default {
                 dataType: 'json',
                 success: function(data, status, xhr) {
                     if (data.features && data.features.length > 0) {
-                        //console.log('Mapbox!');
-                        //console.log(data.features[0]);
                         var view = vm.olmap.getView();
                         view.animate({
                             center: ol.proj.fromLonLat(data.features[0].geometry.coordinates),
@@ -719,7 +708,6 @@ export default {
                 // when you pass control of the popup element to OpenLayers :(
                 $("#mapPopupName")[0].innerHTML = feature.get('name');
                 if (feature.get('images')) {
-                    // console.log('feature.get('images')[0].image ' + feature.get('images')[0].image);
                    $("#mapPopupImage").attr('src', feature.get('images')[0].image);
                     $("#mapPopupImage").show();
                 } else {
@@ -732,32 +720,21 @@ export default {
                 }
                 $("#mapPopupDescription")[0].innerHTML = feature.get('description');
 
-                //Need to change this portion for the new button
-
-                //$("#mapPopupInfo").attr('href', feature.get('info_url'));
-
-               // if/else used to diffrentiate campground type(if covers type 0 and 1) ,diffrentiated at api - backend
+                // Need to change this portion for the new button
+                // if/else used to diffrentiate campground type(if covers type 0 and 1), differentiated by API
                 if (feature.get('campground_type') == 0) {
-
-                $("#mapPopupBook").show();
-                $("#mapPopupBook").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
-                $("#mapPopupInfo").hide();
-                } else if( feature.get('campground_type') == 1 ){
-
-                $("#mapPopupBook").hide();
-                $("#mapPopupInfo").show();
-                $("#mapPopupInfo").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
-
-                } else {
-                  $("#mapPopupInfo").attr('href', feature.get('info_url'));
-                }
-
-               /* if (feature.get('campground_type') == 0) {
                     $("#mapPopupBook").show();
+                    $("#mapPopupInfo").hide();
+                    $("#mapPopupBook").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
+                } else if( feature.get('campground_type') == 1 ){
+                    $("#mapPopupBook").hide();
+                    $("#mapPopupInfo").show();
+                    $("#mapPopupInfo").attr('href', feature.get('info_url'));
                 } else {
                     $("#mapPopupBook").hide();
-                } */
-
+                    $("#mapPopupInfo").show();
+                    $("#mapPopupInfo").attr('href', feature.get('info_url'));
+                }
             }
         },
         groundFilter: function(feature) {
@@ -809,8 +786,6 @@ export default {
             }
         },
         updateDates: function(ev) {
-           // console.log('BANG');
-           // console.log(ev);
             // for the first time someone changes the dates, enable the
             // "Show bookable campsites only" flag
             if (this.dateSetFirstTime) {
@@ -831,7 +806,6 @@ export default {
           let vm = this
              var tempDate = datestring.body;
               vm.currentDate = vm.currentDate = moment({year: tempDate.getFullYear(), month: tempDate.getMonth(), day: tempDate.getDate(), hour: 0, minute: 0, second: 0});
-             // console.log('In setdata ,Function Date currentDate:'+vm.currentDate);
 
         },
 
@@ -919,7 +893,7 @@ export default {
         var today = moment.utc().add(8, 'hours');
         today = moment.utc({year: today.year(), month: today.month(), day: today.date()})
 
-        //End of change
+        // End of change
 
         this.arrivalEl = $('#dateArrival');
         this.departureEl = $('#dateDeparture');
@@ -936,10 +910,8 @@ export default {
                 //return '';
             }
         }).on('changeDate', function (ev) {
-           // console.log('arrivalEl changeDate');
             ev.target.dispatchEvent(new CustomEvent('change'));
         }).on('change', function (ev) {
-
             if (vm.arrivalData.date.valueOf() >= vm.departureData.date.valueOf()) {
                 var newDate = moment(vm.arrivalData.date).add(1, 'days').toDate();
                 vm.departureData.date = newDate;
@@ -948,9 +920,7 @@ export default {
                 vm.departureEl.trigger('changeDate');
 
             }
-
             vm.arrivalDate = moment(vm.arrivalData.date);
-
         }).on('keydown', function (ev) {
             if (ev.keyCode == 13) {
                 ev.target.dispatchEvent(new CustomEvent('change'));
@@ -963,7 +933,6 @@ export default {
                 return (date.valueOf() <= vm.arrivalData.date.valueOf()) ? 'disabled': '';
             }
         }).on('changeDate', function (ev) {
-            //console.log('departureEl changeDate');
             ev.target.dispatchEvent(new CustomEvent('change'));
         }).on('change', function (ev) {
             vm.departureData.hide();
@@ -986,8 +955,6 @@ export default {
             success: function (response, stat, xhr) {
                 vm.suggestions = response;
                 $(search).on('awesomplete-selectcomplete', function(ev) {
-                    //console.log('autoselect');
-                    //console.log(ev);
                     this.blur();
                     //vm.search(ev.target.value);
                 });
@@ -1000,8 +967,6 @@ export default {
 
         // wire up search box
         $(search).on('blur', function(ev) {
-            //console.log('blur');
-            //console.log(ev);
             vm.search(ev.target.value);
         }).on('keypress', function(ev) {
             if (!ev) {
@@ -1010,7 +975,6 @@ export default {
             // intercept enter keys
             var keyCode = ev.keyCode || ev.which;
             if (keyCode == '13') {
-                //console.log('enter');
                 this.blur();
                 return false;
             }
@@ -1145,7 +1109,6 @@ export default {
                     });
                     feature.set('style', style);
                 }
-                //console.log(style);
                 return style;
             }
         });
@@ -1249,7 +1212,6 @@ export default {
                 // when you pass control of the popup element to OpenLayers :(
                 $("#mapPopupName")[0].innerHTML = feature.get('name');
                 if (feature.get('images')) {
-                    // console.log(feature.get('images')[0].image);
                     $("#mapPopupImage").attr('src', feature.get('images')[0].image);
                     $("#mapPopupImage").show();
                 } else {
@@ -1264,33 +1226,21 @@ export default {
 
                 // This portion needs to be modified to accomodate the new button
                 // Online/Offline sites is determined by the backend api
-               if (feature.get('campground_type') == 0) {
-
-                $("#mapPopupBook").show()
-                $("#mapPopupInfo").hide()
-                $("#mapPopupBook").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
-
-                } else if (feature.get('campground_type') == 1 ) {
-
-                $("#mapPopupBook").hide ()
-                $("#mapPopupInfo").show()
-                $("#mapPopupInfo").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
-
-                }
-                // Now,this section is used for the partner accomadation
-                else {
-                $("#mapPopupInfo").show()
-                $("#mapPopupBook").hide()
-                $("#mapPopupInfo").attr('href', feature.get('info_url'));
-                }
-
-                /* $("#mapPopupInfo").attr('href', feature.get('info_url'));
-                $("#mapPopupBook").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
                 if (feature.get('campground_type') == 0) {
-                    $("#mapPopupBook").show();
-                } else {
-                    $("#mapPopupBook").hide();
-                } */
+                    $("#mapPopupBook").show()
+                    $("#mapPopupInfo").hide()
+                    $("#mapPopupBook").attr('href', vm.parkstayUrl+'/availability/?site_id='+feature.getId()+'&'+vm.bookingParam);
+                } else if (feature.get('campground_type') == 1 ) {
+                    $("#mapPopupBook").hide ()
+                    $("#mapPopupInfo").show()
+                    $("#mapPopupInfo").attr('href', feature.get('info_url'));
+                }
+                else {
+                    // Now,this section is used for the partner accommodation
+                    $("#mapPopupBook").hide()
+                    $("#mapPopupInfo").show()
+                    $("#mapPopupInfo").attr('href', feature.get('info_url'));
+                }
 
                 return true;
             }, {
