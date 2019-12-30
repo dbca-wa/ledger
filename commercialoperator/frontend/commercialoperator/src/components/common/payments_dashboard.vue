c3b3306cccdbeec6bb42b9704728c6f8cf75741f
dpawasi <asi@dpaw.wa.gov.au>
2019-12-30 16:54:41 +0800

    fixed .pdf ext for invoice/confirmation url link, minor fix ...

--- commercialoperator/frontend/commercialoperator/src/components/common/payments_dashboard.vue
+++ commercialoperator/frontend/commercialoperator/src/components/common/payments_dashboard.vue
@@ -267,7 +267,7 @@ export default {
                         mRender:function (data,type,full) {
                             let links = '';
                             if(vm.is_payment_admin){
-                                if ( (full.payment_status.toLowerCase()=='paid' || full.payment_method=='Credit Card') && vm.is_internal) {
+                                if ( (full.payment_status.toLowerCase()=='paid' || full.payment_status.toLowerCase()=='over_paid') ) {
                                     links +=  `<a href='/ledger/payments/invoice/payment?invoice=${full.invoice_reference}' target='_blank'>View Payment</a><br/>`;
                                 } else {
                                     links +=  `<a href='/ledger/payments/invoice/payment?invoice=${full.invoice_reference}' target='_blank'>Record Payment</a><br/>`;
