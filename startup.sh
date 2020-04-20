94c291248001422af4ad1136ce711b8317d2da0c
Jason Moore <jason.moore@dbca.wa.gov.au>
2020-04-20 18:28:12 +0800

    Docker Cron Example

--- startup.sh
+++ startup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Start the first process
-./my_first_process -D
+env > /etc/.cronenv
+
+service cron start &
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Failed to start my_first_process: $status"
+  echo "Failed to start gunicorn: $status"
   exit $status
 fi
 
 # Start the second process
-./my_second_process -D
+gunicorn ledgergw.wsgi --bind :8080 --config /app/gunicorn.ini
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Failed to start my_second_process: $status"
+  echo "Failed to start cron: $status"
   exit $status
 fi
 
@@ -22,14 +24,14 @@ fi
 # if it detects that either of the processes has exited.
 # Otherwise it loops forever, waking up every 60 seconds
 
-while sleep 60; do
-  ps aux |grep my_first_process |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep my_second_process |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit 1
-  fi
+#while sleep 60; do
+#  ps aux |grep my_first_process |grep -q -v grep
+#  PROCESS_1_STATUS=$?
+#  ps aux |grep my_second_process |grep -q -v grep
+#  PROCESS_2_STATUS=$?
+#  # If the greps above find anything, they exit with 0 status
+#  # If they are not both 0, then something is wrong
+#  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+#    echo "One of the processes has already exited."
+#    exit 1
+#  fi
