81e34c194516017aacd918f40b02fe14113a834f
Jason Moore <jason.moore@dbca.wa.gov.au>
2020-08-18 12:35:33 +0800

    Fix DB Router

--- ledger/payments/models.py
+++ ledger/payments/models.py
@@ -95,13 +95,19 @@ class OracleOpenPeriod(models.Model):
         managed = False
         db_table = 'payments_open_periods'
 
-
 class OracleFinanceDBRouter(object):
 
     def db_for_read(self, model, **hints):
        if model._meta.db_table == 'payments_open_periods' or model._meta.db_table == 'payments_account_codes':
            return 'oracle_finance'
-       return 'default'
+       return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write auth and contenttypes models go to auth_db.
+        """
+        return None
+
 #     def db_for_read(self, model, **hints):
 #         if model._meta.app_label == 'oracle_finance':
 #             return 'oracle_finance'
