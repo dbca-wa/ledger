894b6b1d730a23af1d3f12cd6fe5040a87bc003f
Jason Moore <jason.moore@dbca.wa.gov.au>
2021-03-05 12:38:26 +0800

    Random hash for cache purge fix

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@
 # Start the first process
 env > /etc/.cronenv
 sed -i 's/\"/\\"/g' /etc/.cronenv
+cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 service cron start &
 status=$?
