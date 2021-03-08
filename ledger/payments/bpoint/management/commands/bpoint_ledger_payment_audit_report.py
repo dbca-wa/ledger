850f355460168489a2a23d4fcfb4f5182d95dc27
Jason Moore <jason@digitalreach.com.au>
2021-03-08 17:08:02 +0800

    Ledger Payment discrepancy report finder

--- ledger/payments/bpoint/management/commands/bpoint_ledger_payment_audit_report.py
+++ ledger/payments/bpoint/management/commands/bpoint_ledger_payment_audit_report.py
@@ -30,11 +30,15 @@ class Command(BaseCommand):
            settlement_date_search = yesterday.strftime("%Y%m%d")
            if options['settlement_date']:
                settlement_date_search = options['settlement_date']
+           settlement_date_search_obj = datetime.strptime(settlement_date_search, '%Y%m%d')
+
            print ("Checking :"+settlement_date_search)
            bpoint_facade = Facade()
            b = bpoint_facade.fetch_transaction_by_settlement_date(settlement_date_search)
            ledger_payment_amount = 0
            bpoint_amount = 0
+           recordcount = 0
+           duplicate_check_array =[]
            for c in b:
                 if c.bank_response_code == '00':
                     amount = str(c.amount)[:-2]+'.'+str(c.amount)[-2:]
@@ -42,25 +46,63 @@ class Command(BaseCommand):
                         bpoint_amount = bpoint_amount - c.amount
                     else:
                         bpoint_amount = bpoint_amount + c.amount
+                    
                     bpoint_amount_nice = str(bpoint_amount)[:-2]+'.'+str(bpoint_amount)[-2:]
                     bp_lpb_diff = bpoint_amount_nice
-                    bp = BpointTransaction.objects.filter(crn1=c.crn1)
+                    bp = BpointTransaction.objects.filter(crn1=c.crn1, action=c.action)
+                    ledger_payment_settlement_date = ''
                     if bp.count() > 0:
+                        ledger_payment_settlement_date = bp[0].settlement_date 
                         if bp[0].action == 'refund':
                             ledger_payment_amount = ledger_payment_amount - bp[0].amount
                         else:
                             ledger_payment_amount = ledger_payment_amount + bp[0].amount
                     bp_lpb_diff = float(bpoint_amount_nice) - float(ledger_payment_amount)
-                    rows.append({'txn_number': c.txn_number,'crn1': c.crn1,'processed_date_time': c.processed_date_time, 'settlement_date': c.settlement_date, 'action': c.action, 'amount': amount, 'bpoint_amount': bpoint_amount_nice, 'ledger_payment_amount': ledger_payment_amount, 'bp_lpb_diff': bp_lpb_diff })
+                    is_dupe = False
+                    if c.crn1 in duplicate_check_array:
+                        is_dupe = True
+                    rows.append({'txn_number': c.txn_number,'crn1': c.crn1,'processed_date_time': c.processed_date_time, 'settlement_date': c.settlement_date, 'action': c.action, 'amount': amount, 'bpoint_amount': bpoint_amount_nice, 'ledger_payment_amount': ledger_payment_amount, 'bp_lpb_diff': bp_lpb_diff ,'ledger_payment_settlement_date': ledger_payment_settlement_date, 'is_dupe': is_dupe})
+                    duplicate_check_array.append(c.crn1)
+                    recordcount=recordcount + 1
+           print ("Transaction count in Bpoint: "+str(recordcount))
+           missing_records = []
+           ledger_bpoint_count = 0
+           bp1 = BpointTransaction.objects.filter(settlement_date=settlement_date_search_obj, crn1__istartswith=SYSTEM_ID)
+           for rec in bp1:
+                  exists = False
+                  for c in b:
+                      if rec.crn1 == c.crn1:
+                          exists = True
+                  if exists is False:
+                         missing_records.append({'crn1': rec.crn1, 'created':rec.created, 'settlement_date': rec.settlement_date, 'amount': rec.amount,'action': rec.action})
+                  ledger_bpoint_count = ledger_bpoint_count + 1
+           print ("Ledger Bpoint Transaction count:" +str(ledger_bpoint_count))
 
-           if  (len(rows)) > 0:
+
+           missing_records_in_ledger = []
+           ledger_bpoint_count = 0
+           bp1 = BpointTransaction.objects.filter(settlement_date=settlement_date_search_obj, crn1__istartswith=SYSTEM_ID)
+           for c in b:
+                  if c.bank_response_code == '00':
+                     exists = False
+                     for rec in bp1:
+                         if rec.crn1 == c.crn1:
+                             exists = True
+                     if exists is False:
+                            missing_records_in_ledger.append({'crn1': c.crn1, 'created':c.processed_date_time, 'settlement_date': c.settlement_date, 'amount':c.amount,'action': c.action})
+                     ledger_bpoint_count = ledger_bpoint_count + 1
+           print ("Ledger Bpoint Transaction count:" +str(ledger_bpoint_count))
+
+           if  (len(rows)) > 0 or (len(missing_records)) > 0 or (len(missing_records_in_ledger)) > 0:
               print ("Sending Report")
               context = {
                   'settlement_date' : settlement_date_search,
-                  'rows' : rows
+                  'rows' : rows,
+                  'missing_records': missing_records,
+                  'missing_records_in_ledger' : missing_records_in_ledger
               }
               email_list = []
               for email_to in settings.NOTIFICATION_EMAIL.split(","):
                      email_list.append(email_to)
-              sendHtmlEmail(tuple(email_list),"[LEDGER] Bpoint Ledger Payment Audit Report "+SYSTEM_ID,context,'email/bpoint_ledger_payment_audit.html',None,None,settings.EMAIL_FROM,'system-oim',attachments=None)
+              sendHtmlEmail(tuple(email_list),"[LEDGER] Bpoint Ledger Payment Audit Report for "+str(settlement_date_search)+" "+SYSTEM_ID,context,'email/bpoint_ledger_payment_audit.html',None,None,settings.EMAIL_FROM,'system-oim',attachments=None)
                     
