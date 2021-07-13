c4550230fa0ae6bb90ea8cdda55ac6eff127d37e
Jason Moore <jason.moore@dbca.wa.gov.au>
2021-07-13 14:10:57 +0800

    Department Users and EmailUser Lookup JSON

---
+++ ledger/accounts/management/commands/test_departmentusers.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from ledger.address.models import UserAddress
+from ledger.accounts import common 
+
+class Command(BaseCommand):
+    help = 'Test Department Users.'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        print (common.retrieve_department_users())
+        print (common.get_department_user('jason.moore@dbca.wa.gov.au'))
+        print (common.get_department_user('jason.moore'))
+        print (common.get_department_user('jason@austwa.com'))
+
+
+
