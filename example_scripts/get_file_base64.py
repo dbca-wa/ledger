a089b7404acb47e6709faec7dd03519dee0e4dc0
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-04-04 14:09:20 +0800

    Clean up example scripts

--- example_scripts/get_file_base64.py
+++ example_scripts/get_file_base64.py
@@ -4,7 +4,7 @@ import mimetypes
 import json
 
 # configs
-api_key='QYJVB595A6VLM4O50L9KMD7GXET2FS3IOP1WW30KBFI8K50KEPA56H08VYTU4SV0BWD4GLU6MPYGS9MBRZQWV70Y46XZUQUW2K8Y'
+api_key='<api key>'
 url = 'http://172.17.0.3:9002/ledgergw/remote/documents/get/'+api_key+'/'
 myobj = {'private_document_id': 15}
 # send request to server to get file
