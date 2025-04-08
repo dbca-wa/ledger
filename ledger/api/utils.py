f56b0c406a9c3c5dc0c537fd88ba8ca6ba70e0e9
Jason Moore <jason.moore@dbca.wa.gov.au>
2025-04-08 13:36:31 +0800

    Fix for correct ip lookup

--- ledger/api/utils.py
+++ ledger/api/utils.py
@@ -5,11 +5,15 @@ import ipaddress
 def get_client_ip(request):
     print ("get_client_ip")
     print (request.META)
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+    x_original_forwarded_for = request.META.get('HTTP_X_ORIGINAL_FORWARDED_FOR')
+    if x_original_forwarded_for:
+        ip = x_original_forwarded_for.split(',')[0]
     else:
-        ip = request.META.get('REMOTE_ADDR')
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
     return ip
 
 
