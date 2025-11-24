bd8dd9c4ccc439fbc3c335471909dcd6e952aa69
Jason Moore <jason.moore@dbca.wa.gov.au>
2025-11-24 17:51:25 +0800

    Invoice Audit Report

---
+++ ledger/payments/bpoint/management/commands/invoice_oracle_audit_report_segregated.py
@@ -0,0 +1,123 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from ledger.payments.bpoint.models import BpointTransaction, BpointToken
+from ledger.payments.models import Invoice,OracleInterface,CashTransaction
+from ledger.order.models import Order, Line as OrderLine
+#from oscar.apps.order.models import Order
+#from ledger.basket.models import Basket
+from django.conf import settings
+from datetime import timedelta, datetime
+from ledger.payments.bpoint.facade import Facade
+from ledger.payments.bpoint.gateway import Gateway
+from ledger.payments.models import OracleInterfaceSystem, OracleInterface, OracleParser, OracleParserInvoice 
+from ledger.emails.emails import sendHtmlEmail
+from ledger.payments import models as payment_models
+from ledger.payments.models import TrackRefund, LinkedInvoice, OracleAccountCode, RefundFailed, OracleInterfaceSystem, OracleParserInvoice
+
+from decimal import Decimal as D
+import json
+import os
+
+class Command(BaseCommand):
+    help = 'Check for payment which have been completed but are missing a booking.'
+
+    def add_arguments(self, parser):
+         yesterday = datetime.today() - timedelta(days=1)
+         settlement_date_search = yesterday.strftime("%Y%m%d")
+         parser.add_argument('settlement_date', nargs='?', default=settlement_date_search)
+         parser.add_argument('system_id', nargs='?', default=None)
+
+    def handle(self, *args, **options):
+
+            SYSTEM_ID = ''
+            ois = None
+            invoice_store_obj = {}
+            print (options)
+            if options['system_id']:
+                    ois = payment_models.OracleInterfaceSystem.objects.filter(integration_type='bpoint_api',enabled=True, system_id=options['system_id'])
+            else:
+                    ois = payment_models.OracleInterfaceSystem.objects.filter(integration_type='bpoint_api',enabled=True)
+                
+            for oracle_system in ois:
+                print (oracle_system)
+                linked_invoice_group_totals = {}
+                rows = []               
+                SYSTEM_ID = oracle_system.system_id 
+
+                yesterday = datetime.today() - timedelta(days=1)
+                settlement_date_search = yesterday.strftime("%Y%m%d")
+                if options['settlement_date']:
+                    settlement_date_search = options['settlement_date']
+                settlement_date_search_obj = datetime.strptime(settlement_date_search, '%Y%m%d')
+                
+                audit_totals = {}
+                oracle_invoices = OracleParserInvoice.objects.filter(parser__date_parsed=settlement_date_search_obj,reference__startswith=oracle_system.system_id)
+                # print (oracle_invoices)
+                for oi in oracle_invoices:
+                    oracle_invoice_total = 0
+                    # print (oi.reference)
+                    #print (oi.details)
+                    if oi.reference in invoice_store_obj:
+                         pass
+                    else:
+                        audit_totals[oi.reference] = {"oracle_total": 0, "invoice_total": 0}
+                    details_json = json.loads(oi.details)
+
+                    for id_key in details_json.keys():
+                        #print (id_key)
+                        #print (details_json[id_key]['order'])
+                        oracle_invoice_total = oracle_invoice_total + float(details_json[id_key]['order'])
+                        #for oracle_code_key in details_json[id_key].keys():
+                        #    print (oracle_code_key)
+                        #print ("INV")
+                        invoice_obj = Invoice.objects.get(reference=oi.reference)
+                        #print (invoice_obj.amount)
+                        #print (oracle_invoice_total)
+                    oracle_invoice_in_decimal = D("{:.2f}".format(oracle_invoice_total))
+                    audit_totals[oi.reference]["oracle_total"] = oracle_invoice_in_decimal
+                    audit_totals[oi.reference]["invoice_total"] = invoice_obj.amount
+                    if oracle_invoice_in_decimal != invoice_obj.amount:
+                        print ("Descrpency")
+                        print (oi.reference)
+                        print (audit_totals[oi.reference])
+
+                # print (audit_totals)
+
+
+                # invoices = Invoice.objects.filter(settlement_date=settlement_date_search_obj, reference__startswith=oracle_system.system_id)
+                # invoices = Invoice.objects.filter(settlement_date=settlement_date_search_obj)
+
+                #linked_invoices = LinkedInvoice.objects.filter(invoice_group_id=68682).values('invoice_group_id').distinct()
+                    
+                # for inv in invoices:
+                #     print (inv.amount)
+                    # orders = Order.objects.filter(number=inv.order_number)
+                    # for o in orders:
+                    #     order_lines = OrderLine.objects.filter(order=o)
+                    #     for ol in order_lines:
+                    #         if 'order' in ol.payment_details:
+                    #             for pd_key,pd_val in ol.payment_details['order'].items():
+                    #                 linked_invoice_group_totals[li['invoice_group_id']]["oracle_order_total"] = linked_invoice_group_totals[li['invoice_group_id']]["oracle_order_total"] + D(pd_val)
+
+                # rows = []
+                # for ligt in linked_invoice_group_totals:
+                #     if linked_invoice_group_totals[ligt]["bpoint_total"] == linked_invoice_group_totals[ligt]["oracle_order_total"]:
+                #         pass
+                #     else:
+                #         row = {"invoice_group_id" : ligt, "bpoint_total": linked_invoice_group_totals[ligt]["bpoint_total"], "oracle_order_total": linked_invoice_group_totals[ligt]["oracle_order_total"], "invoices": linked_invoice_group_totals[ligt]["invoices"]}
+                #         rows.append(row)
+
+                # if (len(rows)) > 0: # or (len(missing_records)) > 0 or (len(missing_records_in_ledger)) > 0:
+                #     print ("Sending Report")
+                #     context = {
+                #         'settlement_date' : settlement_date_search,
+                #         'rows' : rows,
+                #     }
+                #     print (context)
+                #     email_list = []
+                #     oirr = payment_models.OracleInterfaceReportReceipient.objects.filter(system=oracle_system)
+                #     for rr in oirr:
+                #             print (rr.email)
+                #             email_list.append(rr.email)
+                #     sendHtmlEmail(tuple(email_list),"[LEDGER] "+oracle_system.system_name+" Ledger Payment Oracle Order Discrepancy Report for "+str(settlement_date_search)+" "+SYSTEM_ID,context,'email/bpoint_ledger_oracle_audit.html',None,None,settings.EMAIL_FROM,'system-oim',attachments=None)
+                        
