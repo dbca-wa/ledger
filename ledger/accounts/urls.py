644f00734f4bb9123d53c953ab52cb139356b4c2
Ash <ash.woodiss-field@dbca.wa.gov.au>
2026-06-09 11:25:48 +0800

    Update: keep app_name

--- ledger/accounts/urls.py
+++ ledger/accounts/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 from ledger.accounts import views
 from ledger.accounts.api import UserReportView,UserAccountsList,UserAccountsLogsList
 
+app_name = 'ledger.accounts'
+
 api_patterns = [
     re_path(r'api/report/duplicate_identity$', UserReportView.as_view(),name='ledger-user-report'),
     re_path(r'api/account/list$', UserAccountsList.as_view(),name='ledger-user-account'),
